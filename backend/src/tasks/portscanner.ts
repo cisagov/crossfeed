bd1624cf810beff20a6c6b46442d3b168b02d502
Jack Cable <jack@dds.mil>
2020-06-25 21:57:49 -0500

    Add POC port scanner

---
+++ backend/src/tasks/portscanner.ts
@@ -0,0 +1,35 @@
+// This is a temporary proof-of-concept port scanner that actively scans ports.
+// We'll want to replace this with the Project Sonar passive port scanner.
+
+import { Handler } from 'aws-lambda';
+import { connectToDatabase, Domain, Service } from '../models';
+import { saveServicesToDb } from './helpers';
+import { plainToClass } from 'class-transformer';
+import { Like } from 'typeorm';
+import * as portscanner from 'portscanner';
+
+export const handler: Handler = async (event) => {
+  await connectToDatabase();
+
+  const domains = await Domain.find({ reverseName: Like('gov.cisa%') });
+  const services: Service[] = [];
+  for (const domain of domains) {
+    for (const port of [21, 22, 80, 443, 3000, 8080, 8443]) {
+      const status = await portscanner.checkPortStatus(port, domain.ip);
+      console.log(status);
+      if (status == 'open') {
+        services.push(
+          plainToClass(Service, {
+            domain: domain,
+            port: port,
+            lastSeen: new Date()
+          })
+        );
+      }
+    }
+  }
+
+  await saveServicesToDb(services);
+
+  console.log(`Portscan updated for ${services.length} new services`);
+};
