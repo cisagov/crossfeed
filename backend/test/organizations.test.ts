9bced571cfdb728d150c097e13d4c8955c4f6391
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-07-06 10:09:17 -0400

    fix tests

--- backend/test/organizations.test.ts
+++ backend/test/organizations.test.ts
@@ -3,28 +3,33 @@ import { BACKEND_URL } from './constants';
 
 describe('organizations', () => {
   it('add new organization', async () => {
+    const name = 'cisa-test-' + Math.random();
     const response = await request(BACKEND_URL)
       .post('/organizations/')
       .send({
         ipBlocks: [],
-        name: 'cisa-test-8697283f-8f04-4b99-8caf-4270da0e678c',
-        rootDomains: ['cisa.gov']
+        name,
+        rootDomains: ['cisa.gov'],
+        isPassive: false
       })
       .expect(200);
     expect(response.body).toMatchSnapshot({
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
-      id: expect.any(String)
+      id: expect.any(String),
+      name: expect.any(String)
     });
+    expect(response.body.name).toEqual(name);
   });
   it("can't add organization with the same name", async () => {
-    const name = 'cisa-test-8697283f-8f04-4b99-8caf-4270da0e678d';
+    const name = 'cisa-test-' + Math.random();
     await request(BACKEND_URL)
       .post('/organizations/')
       .send({
         ipBlocks: [],
         name,
-        rootDomains: ['cisa.gov']
+        rootDomains: ['cisa.gov'],
+        isPassive: false
       })
       .expect(200);
     const response = await request(BACKEND_URL)
@@ -32,7 +37,8 @@ describe('organizations', () => {
       .send({
         ipBlocks: [],
         name,
-        rootDomains: ['cisa.gov']
+        rootDomains: ['cisa.gov'],
+        isPassive: false
       })
       .expect(500);
     expect(response.body).toMatchSnapshot();
