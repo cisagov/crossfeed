84f13f08ee77ee5124c7398630962f0bb6b85ae6
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-07-12 11:49:41 -0400

    add public subnet to output

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -2,6 +2,10 @@ output "lambda_subnet_id" {
   value = aws_subnet.lambda_subnet.id
 }
 
+output "lambda_subnet_public_id" {
+  value = aws_subnet.lambda_subnet2.id
+}
+
 output "lambda_sg_id" {
   value = aws_security_group.allow_internal.id
 }
