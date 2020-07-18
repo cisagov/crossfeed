6c6e66f1544e87a98b08223fff2f526c660e3d51
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-07-18 15:28:14 -0400

    fix lint

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '../.env' });
 process.env.BACKEND_URL = '';
 process.env.DB_HOST = 'localhost';
-process.env.DB_NAME = 'crossfeed_test'
+process.env.DB_NAME = 'crossfeed_test';
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
