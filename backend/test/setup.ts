d62a7d8ccdf12f593f0e57eff124316bbf2be442
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-07-18 15:32:30 -0400

    clear db before every test

--- backend/test/setup.ts
+++ backend/test/setup.ts
@@ -2,6 +2,6 @@ import { handler as syncdb } from '../src/tasks/syncdb';
 
 export default async () => {
   console.warn('Syncing test db...');
-  await (syncdb as any)({});
+  await (syncdb as any)("dangerouslyforce");
   console.warn('Done syncing test db.');
 };
