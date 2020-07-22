0429aec9ad99ad2fe5702ba7f1319c12fae2332a
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-07-22 11:54:12 -0400

    Bundle backend with webpack

---
+++ backend/webpack.backend.config.js
@@ -0,0 +1,32 @@
+const slsw = require('serverless-webpack');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  optimization: {
+    minimize: false
+  },
+  target: 'node',
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: [/\.tsx?$/],
+        exclude: [/node_modules/, /\.test.tsx?$/],
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^pg-native$/),
+    new webpack.IgnorePlugin(/^canvas$/) // imported by jsdom from simple-wapplyzer, not used so we can ignore.
+  ],
+  resolve: {
+    modules: ['node_modules', 'scripts'],
+    extensions: ['.ts', '.tsx', '.json', '.js', '.jsx']
+  }
+};
