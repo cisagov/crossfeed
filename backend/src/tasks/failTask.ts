8b7d7643c1390ba7062c2709ae9ba81e1462ff11
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-07-22 10:40:13 -0400

    fix

--- backend/src/tasks/failTask.ts
+++ backend/src/tasks/failTask.ts
@@ -4,7 +4,7 @@ import { connectToDatabase, ScanTask } from '../models';
 export const handler: Handler = async (event) => {
   const connection = await connectToDatabase(true);
   console.log(event);
-  if (event.email) {
+  if (event.id) {
     const scanTask = await ScanTask.findOne(event.id);
     if (scanTask) {
       scanTask.status = 'failed';
