51d8858442403c39c7df32de2dc98c8cfb5aadd9
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-07-28 14:52:23 -0400

    Fix worker deploy script
    Fix the path of "build-worker.sh".

--- backend/tools/deploy-worker.sh
+++ backend/tools/deploy-worker.sh
@@ -6,7 +6,7 @@ set -e
 
 AWS_ECR_DOMAIN=563873274798.dkr.ecr.us-east-1.amazonaws.com
 
-./build-worker.sh
+./tools/build-worker.sh
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ECR_DOMAIN
 docker tag crossfeed-worker:latest $AWS_ECR_DOMAIN/crossfeed-staging-worker:latest
 docker push $AWS_ECR_DOMAIN/crossfeed-staging-worker:latest
