24420672c19f96848577b2aad9bb47b188b02285
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-07-30 21:30:43 -0400

    Add test

--- backend/test/users.test.ts
+++ backend/test/users.test.ts
@@ -79,9 +79,53 @@ describe('user', () => {
       expect(response.body.roles[0].approved).toEqual(true);
       expect(response.body.roles[0].role).toEqual('user');
     });
-    it('invite existing user by a different organization admin should work, and should modify user name, but not any other details', async () => {
+    it('invite existing user by a different organization admin should work, and should not modify other user details', async () => {
       const firstName = 'first name';
       const lastName = 'last name';
+      const email = Math.random() + '@crossfeed.cisa.gov';
+      const user = await User.create({
+        firstName: '',
+        lastName: '',
+        email
+      }).save();
+      await Role.create({
+        role: 'user',
+        approved: false,
+        organization,
+        user
+      }).save();
+      const response = await request(app)
+        .post('/users')
+        .set(
+          'Authorization',
+          createUserToken({
+            roles: [
+              {
+                org: organization2.id,
+                role: 'admin'
+              }
+            ]
+          })
+        )
+        .send({
+          firstName,
+          lastName,
+          email,
+          organization: organization2.id,
+          organizationAdmin: false
+        })
+        .expect(200);
+      expect(response.body.id).toEqual(user.id);
+      expect(response.body.email).toEqual(email);
+      expect(response.body.invitePending).toEqual(false);
+      expect(response.body.firstName).toEqual('first name');
+      expect(response.body.lastName).toEqual('last name');
+      expect(response.body.roles[1].approved).toEqual(true);
+      expect(response.body.roles[1].role).toEqual('user');
+    });
+    it('invite existing user by a different organization admin should work, and should modify user name if user name is initially blank', async () => {
+      const firstName = '';
+      const lastName = '';
       const email = Math.random() + '@crossfeed.cisa.gov';
       const user = await User.create({
         firstName: '',
