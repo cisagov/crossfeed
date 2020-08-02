9b1bb723cd79146729fc33528012fd249add81c5
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-08-02 11:24:49 -0400

    add more snapshot length

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,6 +3,9 @@ process.env.BACKEND_URL = '';
 process.env.DB_HOST = 'localhost';
 process.env.DB_NAME = 'crossfeed_test';
 process.env.IS_LOCAL = 'true';
+process.env.CENSYS_API_ID = 'CENSYS_API_ID';
+process.env.CENSYS_API_SECRET = 'CENSYS_API_SECRET';
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
