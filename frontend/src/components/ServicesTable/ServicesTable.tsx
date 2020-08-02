83d411c50cf0ae09fddc3967c1a576f3496a9b23
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-08-02 10:45:54 -0400

    censys - properly show banners

--- frontend/src/components/ServicesTable/ServicesTable.tsx
+++ frontend/src/components/ServicesTable/ServicesTable.tsx
@@ -14,28 +14,31 @@ const CodeBlock: React.FC<{}> = ({ children }) => {
 };
 
 export const ServicesTable: React.FC<Props> = ({ services }) => {
-  const renderExpanded = useCallback((row: Row<Service>) => {
-    const { original } = row;
-    return (
-      <div className={classes.expandedRoot}>
-        {original.banner && (
-          <>
-            <h4>Banner</h4>
-            <CodeBlock>{original.banner}</CodeBlock>
-          </>
-        )}
-        {original.censysIpv4Results &&
-          Object.keys(original.censysIpv4Results)?.length === 0 && (
+  const renderExpanded = useCallback(
+    (row: Row<Service>) => {
+      const { original } = row;
+      return (
+        <div className={classes.expandedRoot}>
+          {original.banner && (
             <>
-              <h4>Censys IPv4 Results</h4>
-              <CodeBlock>
-                {JSON.stringify(original.censysIpv4Results, null, 2)}
-              </CodeBlock>
+              <h4>Banner</h4>
+              <CodeBlock>{original.banner}</CodeBlock>
             </>
           )}
-      </div>
-    );
-  }, []);
+          {original.censysIpv4Results &&
+            Object.keys(original.censysIpv4Results)?.length > 0 && (
+              <>
+                <h4>Censys IPv4 Results</h4>
+                <CodeBlock>
+                  {JSON.stringify(original.censysIpv4Results, null, 2)}
+                </CodeBlock>
+              </>
+            )}
+        </div>
+      );
+    },
+    [classes]
+  );
 
   return (
     <Table<Service>
