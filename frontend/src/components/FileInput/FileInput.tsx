e3516f12029b9a61cb544ab42cc8174f8a58ade2
Jack Cable <jack.cable@cisa.dhs.gov>
2020-08-06 14:44:59 -0500

    Update wording, delete commented section

--- frontend/src/components/FileInput/FileInput.tsx
+++ frontend/src/components/FileInput/FileInput.tsx
@@ -4,10 +4,7 @@ export const FileInput = (props: InputHTMLAttributes<HTMLInputElement>) => (
   <div className="usa-file-input">
     <div className="usa-file-input__target">
       <div className="usa-file-input__instructions" aria-hidden="true">
-        {/* <span className="usa-file-input__drag-text">Drag file here or </span>
-        <span className="usa-file-input__choose">choose from folder</span> */}
-
-        <span className="usa-file-input__choose">Choose from folder</span>
+        <span className="usa-file-input__choose">Choose a file</span>
       </div>
       <div className="usa-file-input__box"></div>
       <input className="usa-file-input__input" type="file" {...props} />
