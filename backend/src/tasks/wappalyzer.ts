9b72d2e4d7b3cefe9be24539f9413656ade58d8d
Jeff Hackshaw <jeffrey.hackshaw@gmail.com>
2020-08-13 20:29:17 -0400

    make wappalyzer run concurrently and improve unit tests (#258)
    * make wappalyzer run concurrently and improve unit tests
    
    * limit concurrency of wappalyzer with p-limit
    
    * use p-queue to limit concurrency and nock for tests
    
    * removed unused axios mock response object

--- backend/src/tasks/wappalyzer.ts
+++ backend/src/tasks/wappalyzer.ts
@@ -1,10 +1,36 @@
 import { Domain } from '../models';
 import { plainToClass } from 'class-transformer';
-import * as wappalyzer from 'simple-wappalyzer';
+import wappalyzer from 'simple-wappalyzer';
 import axios from 'axios';
 import { CommandOptions } from './ecs-client';
 import getLiveWebsites from './helpers/getLiveWebsites';
 import saveDomainsToDb from './helpers/saveDomainsToDb';
+import PQueue from 'p-queue';
+
+const wappalyze = async (domain: Domain): Promise<Domain | undefined> => {
+  const ports = domain.services.map((service) => service.port);
+  const url = ports.includes(443)
+    ? `https://${domain.name}`
+    : `http://${domain.name}`;
+  try {
+    const { data, status, headers } = await axios.get(url, {
+      validateStatus: () => true
+    });
+    const result = await wappalyzer({ url, data, status, headers });
+    if (result.length > 0) {
+      return plainToClass(Domain, {
+        name: domain.name,
+        webTechnologies: result
+      });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const filterEmpty = <T>(value: T | undefined | null): value is T => {
+  return value !== null && value !== undefined;
+};
 
 export const handler = async (commandOptions: CommandOptions) => {
   const { organizationId, organizationName } = commandOptions;
@@ -12,32 +38,12 @@ export const handler = async (commandOptions: CommandOptions) => {
   console.log('Running wappalyzer on organization', organizationName);
 
   const liveWebsites = await getLiveWebsites(organizationId!);
-  const domains: Domain[] = [];
-  for (const domain of liveWebsites) {
-    const url =
-      (domain.services.map((service) => service.port).includes(443)
-        ? 'https://'
-        : 'http://') + domain.name;
-    try {
-      const { data, status, headers } = await axios.get(url, {
-        validateStatus: function () {
-          // Never throw error on non-200 response
-          return true;
-        }
-      });
-      const result = await wappalyzer({ url, data, status, headers });
-      if (result.length == 0) continue;
-      domains.push(
-        plainToClass(Domain, {
-          name: domain.name,
-          webTechnologies: result
-        })
-      );
-    } catch (e) {
-      console.error(e);
-      continue;
-    }
-  }
-  saveDomainsToDb(domains);
+  const queue = new PQueue({ concurrency: 5 });
+  const results: (Domain | undefined)[] = await Promise.all(
+    liveWebsites.map((site) => queue.add(() => wappalyze(site)))
+  );
+  const domains = results.filter(filterEmpty);
+
+  await saveDomainsToDb(domains);
   console.log(`Wappalyzer finished for ${domains.length} domains`);
 };
