fb9eae8846e1ac8bc5f698bcf496e204cb248d8e
Jeff Hackshaw <jeffrey.hackshaw@gmail.com>
2020-08-14 10:40:01 -0400

    control cache via cloudfront configuration over s3 headers

--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -53,8 +53,28 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 0
+    max_ttl                = 0
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/static/*"
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 2628000
+    max_ttl                = 2628000
   }
 
   tags = {	
