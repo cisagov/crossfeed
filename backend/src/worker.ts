ae39fa769d6d4313151b6c21b5c85ee9b5fe0955
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-08-14 22:44:35 -0400

    remove scantask update logic in worker

--- backend/src/worker.ts
+++ backend/src/worker.ts
@@ -12,43 +12,27 @@ import { handler as cve } from './tasks/cve';
  * Worker entrypoint.
  */
 async function main() {
-  await connectToDatabase();
-
   const commandOptions: CommandOptions = JSON.parse(
     process.env.CROSSFEED_COMMAND_OPTIONS || '{}'
   );
   console.log('commandOptions are', commandOptions);
 
-  const { scanName, scanTaskId } = commandOptions;
-  const scanTask = await ScanTask.findOneOrFail(scanTaskId);
+  const { scanName } = commandOptions;
 
-  scanTask.status = 'started';
-  scanTask.startedAt = new Date();
-  await scanTask.save();
-
-  try {
-    const scanFn = {
-      amass,
-      censys,
-      censysIpv4,
-      cve,
-      findomain,
-      portscanner,
-      wappalyzer
-    }[scanName];
-    if (!scanFn) {
-      throw new Error('Invalid scan name ' + scanName);
-    }
-    await scanFn(commandOptions);
-    scanTask.status = 'finished';
-  } catch (e) {
-    console.error(e);
-    scanTask.status = 'failed';
-    scanTask.output = JSON.stringify(e);
-  } finally {
-    scanTask.finishedAt = new Date();
-    await scanTask.save();
+  const scanFn = {
+    amass,
+    censys,
+    censysIpv4,
+    cve,
+    findomain,
+    portscanner,
+    wappalyzer
+  }[scanName];
+  if (!scanFn) {
+    throw new Error('Invalid scan name ' + scanName);
   }
+  
+  await scanFn(commandOptions);
 }
 
 main();
