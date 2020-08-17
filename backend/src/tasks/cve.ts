cacad2bd0ca3dd9e92923a7c795c45bee26a8f7e
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-08-17 09:42:11 -0400

    rm comment

--- backend/src/tasks/cve.ts
+++ backend/src/tasks/cve.ts
@@ -58,7 +58,6 @@ export const handler = async (commandOptions: CommandOptions) => {
     console.log(`${index} ${host.cpes.join(',')}`);
   }
 
-  // Should change this to spawnSync
   const {
     status,
     stdout,
