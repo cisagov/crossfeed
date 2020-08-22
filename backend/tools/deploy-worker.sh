949c4a5454ce5a71607fd89fc4a89ea55dc06547
Jack Cable <jack.cable@cisa.dhs.gov>
2020-08-22 18:51:23 -0500

    Fix deploy worker

--- backend/tools/deploy-worker.sh
+++ backend/tools/deploy-worker.sh
@@ -10,7 +10,7 @@ AWS_ECR_DOMAIN=957221700844.dkr.ecr.us-east-1.amazonaws.com
 
 WORKER_TAG=${1:-crossfeed-staging-worker}
 
-# ./tools/build-worker.sh
+./tools/build-worker.sh
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ECR_DOMAIN
 docker tag crossfeed-worker:latest $AWS_ECR_DOMAIN/$WORKER_TAG:latest
 docker push $AWS_ECR_DOMAIN/$WORKER_TAG:latest
