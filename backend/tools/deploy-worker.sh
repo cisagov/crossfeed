50b79126c7c4490592f4dbd0ab4173714ae06a2e
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-08-22 12:19:01 -0400

    Allow deploying worker to prod (#284)

--- backend/tools/deploy-worker.sh
+++ backend/tools/deploy-worker.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# Deploys worker to terraform.
-# If the worker_ecs_repository_url output from Terraform changes, you should replace "563873274798.dkr.ecr.us-east-1.amazonaws.com" in this file with that URL.
+# Deploys worker to Terraform.
+# If the worker_ecs_repository_url output from Terraform changes, you should replace "XXX.dkr.ecr.us-east-1.amazonaws.com" in this file with that URL.
+# To deploy staging, run ./deploy-worker.sh.
+# To deploy prod, run ./deploy-worker.sh crossfeed-prod-worker.
 
 set -e
 
 AWS_ECR_DOMAIN=957221700844.dkr.ecr.us-east-1.amazonaws.com
 
-./tools/build-worker.sh
+WORKER_TAG=${1:-crossfeed-staging-worker}
+
+# ./tools/build-worker.sh
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ECR_DOMAIN
-docker tag crossfeed-worker:latest $AWS_ECR_DOMAIN/crossfeed-staging-worker:latest
-docker push $AWS_ECR_DOMAIN/crossfeed-staging-worker:latest
+docker tag crossfeed-worker:latest $AWS_ECR_DOMAIN/$WORKER_TAG:latest
+docker push $AWS_ECR_DOMAIN/$WORKER_TAG:latest
