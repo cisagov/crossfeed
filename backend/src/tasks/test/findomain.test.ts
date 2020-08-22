ee73768ac1e13cc8f6285250e743f2ebb2e989ef
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-08-22 11:05:27 -0400

    add findomain test

--- backend/src/tasks/test/findomain.test.ts
+++ backend/src/tasks/test/findomain.test.ts
@@ -61,9 +61,16 @@ describe('findomain', () => {
       ipBlocks: [],
       isPassive: false
     }).save();
+    const scanOld = await Scan.create({
+      name: 'findomain',
+      arguments: {},
+      frequency: 999
+    }).save();
     const domain = await Domain.create({
       organization,
-      name: 'filedrop.cisa.gov'
+      name: 'filedrop.cisa.gov',
+      discoveredBy: scanOld,
+      fromRootDomain: 'oldrootdomain.cisa.gov'
     }).save();
     await findomain({
       organizationId: organization.id,
@@ -74,13 +81,18 @@ describe('findomain', () => {
     });
 
     const domains = await Domain.find({
-      organization
+      where: { organization },
+      relations: ['discoveredBy']
     });
     expect(domains.length).toEqual(2);
     expect(domains[0].id).toEqual(domain.id);
     expect(domains[0].name).toEqual('filedrop.cisa.gov');
     expect(domains[0].ip).toEqual('104.84.119.215');
+    expect(domains[0].discoveredBy.id).toEqual(scanOld.id);
+    expect(domains[0].fromRootDomain).toEqual('oldrootdomain.cisa.gov');
     expect(domains[1].name).toEqual('www.filedrop.cisa.gov');
     expect(domains[1].ip).toEqual('104.84.119.215');
+    expect(domains[1].discoveredBy.id).toEqual(scan.id);
+    expect(domains[1].fromRootDomain).toEqual(organization.rootDomains[0]);
   });
 });
