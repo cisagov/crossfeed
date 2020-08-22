e29bc027ea9767107fe73145bc443443ebdd20f4
Jack Cable <jack.cable@cisa.dhs.gov>
2020-08-22 11:37:27 -0500

    Update terms to v2.1

--- frontend/src/pages/TermsOfUse/TermsOfUse.tsx
+++ frontend/src/pages/TermsOfUse/TermsOfUse.tsx
@@ -18,7 +18,7 @@ interface Errors extends Partial<FormData> {
 export const currentTermsVersion = '1';
 
 export const getMaximumRole = (user: AuthUser | null | undefined) => {
-  if (user?.userType === 'globalView') return 'globalView';
+  if (user?.userType === 'globalView') return 'user';
   return user && user.roles && user.roles.find(role => role.role === 'admin')
     ? 'admin'
     : 'user';
@@ -63,46 +63,82 @@ export const TermsOfUse: React.FC = () => {
         (CISA). Using both passive and active processes, Crossfeed can
         continuously evaluate the cybersecurity posture of your public-facing,
         internet-accessible network assets for vulnerabilities or configuration
-        issues. Once you create a Crossfeed account, input the Internet Protocol
-        (IP) addresses or domains to be continuously evaluated, and select the
-        scanning/evaluation protocols to be used, Crossfeed will collect data
-        about the sites you specified from multiple publicly-available
-        resources, including through active interactions with your sites, if
-        that option is selected by you. Crossfeed will also examine any
-        publicly-available, internet-accessible resources that appear to be
-        related or otherwise associated with IPs or domains you have provided us
-        to evaluate, presenting you with a list of those related sites for your
-        awareness.
+        issues.
       </p>
       <p>
-        By creating a Crossfeed login and using this service, you request CISA’s
-        technical assistance to detect vulnerabilities and configuration issues
-        through Crossfeed and agree to the following:
+        Crossfeed supports two types of users for your organization:
+        administrative users or view-only users. Administrative users can
+        add/delete domains for their organization, schedule and disable scans
+        for their organization, and invite others to create Crossfeed accounts
+        to have access to the organization’s data. View-only users can only view
+        data provided to or collected by Crossfeed.
+      </p>
+      {getMaximumRole(user) === 'admin' && (
+        <p>
+          Once you create a Crossfeed administrator account, input the Internet
+          Protocol (IP) addresses or domains to be continuously evaluated, and
+          select the scanning/evaluation protocols to be used, Crossfeed will
+          collect data about the sites you specified from multiple
+          publicly-available resources, including through active interactions
+          with your sites, if that option is selected by you. Crossfeed will
+          also examine any publicly-available, internet-accessible resources
+          that appear to be related or otherwise associated with IPs or domains
+          you have provided us to evaluate, presenting you with a list of those
+          related sites for your awareness.
+        </p>
+      )}
+      <p>
+        By creating a Crossfeed{' '}
+        {getMaximumRole(user) === 'admin' ? 'administrator' : 'view only'}{' '}
+        account and using this service, you request CISA’s technical assistance
+        to detect vulnerabilities and configuration issues through Crossfeed and
+        agree to the following:
       </p>
       <ul>
+        {getMaximumRole(user) === 'admin' && (
+          <>
+            <li>
+              You have authority to authorize scanning/evaluation of the
+              public-facing networks and systems you submit within Crossfeed and
+              you authorize CISA to conduct any such scans/evaluation through
+              Crossfeed;
+            </li>
+            <li>
+              You agree to promptly update or change the information used to
+              identify the public-facing networks and systems to be
+              scanned/evaluated pursuant to this authorization;
+            </li>
+            <li>
+              You agree to comply with any notification or authorization
+              requirement that any third party that operates or maintains your
+              public-facing networks or systems may impose on external
+              vulnerability scanning services, modifying any Crossfeed scans
+              associated with your account if external scanning of those
+              resources is later prohibited;
+            </li>
+            <li>
+              You accept that, while Crossfeed will use best efforts to conduct
+              scans in a way that minimizes risk to your organization’s systems
+              and networks, Crossfeed scanning activities creates some risk of
+              degradation in performance to your organization’s systems and
+              networks;
+            </li>
+            <li>
+              You agree that CISA may share data gathered by Crossfeed with
+              other federal agencies with cybersecurity responsibilities, with
+              the Multi-State Information Sharing and Analysis Center, and with
+              the Election Infrastructure Information Sharing and Analysis
+              Center.
+            </li>
+            <li>
+              You are authorized to make the above certifications on your
+              organization’s behalf
+            </li>
+          </>
+        )}
         <li>
-          You have authority to authorize scanning/evaluation of the
-          public-facing networks and systems you submit within Crossfeed and you
-          authorize CISA to conduct any such scans/evaluation through Crossfeed;
-        </li>
-        <li>
-          You agree to promptly update or change the information used to
-          identify the public-facing networks and systems to be
-          scanned/evaluated pursuant to this authorization;
-        </li>
-        <li>
-          You agree to comply with any notification or authorization requirement
-          that any third party that operates or maintains your public-facing
-          networks or systems may impose on external vulnerability scanning
-          services, modifying any Crossfeed scans associated with your account
-          if external scanning of those resources is later prohibited;
-        </li>
-        <li>
-          You accept that, while Crossfeed will use best efforts to conduct
-          scans in a way that minimizes risk to your organization’s systems and
-          networks, Crossfeed scanning activities creates some risk of
-          degradation in performance to your organization’s systems and
-          networks;
+          You accept that CISA may modify or discontinue the Crossfeed service
+          at any time;
         </li>
         <li>
           You acknowledge that use of Crossfeed is governed exclusively by
@@ -113,18 +149,14 @@ export const TermsOfUse: React.FC = () => {
           vulnerabilities or configuration issues present in your system;
         </li>
         <li>
-          You accept that CISA may modify or discontinue the Crossfeed service
-          at any time;
-        </li>
-        <li>
           You agree to not:
           <ul>
             <li>
               Use the Crossfeed service in violation of any applicable law;
             </li>
             <li>
-              Access or attempt to access any other user’s Crossfeed account or
-              the data contained in such account; and
+              Access or attempt to access any Crossfeed account other than your
+              own; and
             </li>
             <li>
               Introduce malware to the Crossfeed platform or otherwise impair,
@@ -136,11 +168,7 @@ export const TermsOfUse: React.FC = () => {
         <li>
           You accept that, at CISA’s sole discretion, CISA may terminate or
           suspend your access to the Crossfeed service due to violation of these
-          terms or any other reason; and
-        </li>
-        <li>
-          You are authorized to make the above certifications on your
-          organization’s behalf.
+          terms or any other reason.
         </li>
       </ul>
       <p>ToU version {getToUVersion(user)}</p>
