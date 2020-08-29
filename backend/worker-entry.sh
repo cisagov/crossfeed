50d1634ac0b6b54f66f50d1f19538b1f4f0f3195
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-08-29 12:04:45 -0400

    set up transparent proxy

--- backend/worker-entry.sh
+++ backend/worker-entry.sh
@@ -1,8 +1,26 @@
-pm2 start mitmdump --interpreter none
+# Transparent proxy
+
+iptables-legacy -t nat -A OUTPUT -p tcp -m owner ! --uid-owner mitmproxyuser --dport 80 -j REDIRECT --to-port 8080
+iptables-legacy -t nat -A OUTPUT -p tcp -m owner ! --uid-owner mitmproxyuser --dport 443 -j REDIRECT --to-port 8080
+ip6tables-legacy -t nat -A OUTPUT -p tcp -m owner ! --uid-owner mitmproxyuser --dport 80 -j REDIRECT --to-port 8080
+ip6tables-legacy -t nat -A OUTPUT -p tcp -m owner ! --uid-owner mitmproxyuser --dport 443 -j REDIRECT --to-port 8080
+
+pm2 start --interpreter none sudo -- -u mitmproxyuser -H bash -c 'mitmdump --mode transparent --showhost --set block_global=false --modify-headers /~q/User-Agent/example.org'
+
 wait-port 8080
-cp ~/.mitmproxy/mitmproxy-ca-cert.pem /usr/local/share/ca-certificates/mitmproxy-ca-cert.crt
+cp /home/mitmproxyuser/.mitmproxy/mitmproxy-ca-cert.pem /usr/local/share/ca-certificates/mitmproxy-ca-cert.crt
 update-ca-certificates --fresh
 
-curl https://www.google.com
+curl https://webhook.site/8c99302a-a063-4c8b-9c0b-0ef000485d76
 
-# curl https://webhook.site/8c99302a-a063-4c8b-9c0b-0ef000485d76 ; pm2 logs mitmdump
+pm2 stop all
+
+
+# Explicit proxy
+# pm2 start --interpreter none mitmdump -- --modify-headers /~q/User-Agent/example.org
+# wait-port 8080
+# cp ~/.mitmproxy/mitmproxy-ca-cert.pem /usr/local/share/ca-certificates/mitmproxy-ca-cert.crt
+# update-ca-certificates --fresh
+
+# curl --proxy localhost:8000 https://webhook.site/8c99302a-a063-4c8b-9c0b-0ef000485d76
+# pm2 stop all
