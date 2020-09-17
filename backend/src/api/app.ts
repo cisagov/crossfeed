808373fe0c86e84d171fa4e1b8ca88eef851477c
Jack Cable <jack.cable@cisa.dhs.gov>
2020-09-17 17:26:19 -0500

    Use res.json on error

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -38,7 +38,7 @@ const handlerToExpress = (handler) => async (req, res, next) => {
     res.status(statusCode).json(parsedBody);
   } catch (e) {
     // Not a JSON body
-    res.status(statusCode).send(body);
+    res.status(statusCode).json({ resp: body });
   }
 };
 
