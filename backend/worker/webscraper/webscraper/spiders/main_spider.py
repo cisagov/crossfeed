fb625a9fa00c3002752ce2c71f56a2e25d79819e
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-09-23 10:52:09 -0400

    Set up scrapy

---
+++ backend/worker/webscraper/webscraper/spiders/main_spider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class MainSpider(scrapy.Spider):
+    name = "main"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
