6b5e04774f13918789de69d43c5369d6df643f9e
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-09-28 13:34:31 -0400

    add more cpu / memory to searchSync

--- backend/src/api/scans.ts
+++ backend/src/api/scans.ts
@@ -96,6 +96,8 @@ export const SCAN_SCHEMA: ScanSchema = {
     type: 'fargate',
     isPassive: true,
     global: true,
+    cpu: '1024',
+    memory: '4096',
     description:
       'Syncs records with Elasticsearch so that they appear in search results.'
   },
