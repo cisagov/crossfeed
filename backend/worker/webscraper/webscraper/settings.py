b516d809b593f82bdab713fe36a6c71842319114
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-10-01 21:16:27 -0400

    change AUTOTHROTTLE_TARGET_CONCURRENCY to 2

--- backend/worker/webscraper/webscraper/settings.py
+++ backend/worker/webscraper/webscraper/settings.py
@@ -76,7 +76,7 @@ AUTOTHROTTLE_START_DELAY = 5
 AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 10.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
 # Enable showing throttling stats for every response received:
 AUTOTHROTTLE_DEBUG = False
 
