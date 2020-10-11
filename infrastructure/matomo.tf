cebef2760a58cf61f107e97342524016a347968e
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-10-10 21:13:10 -0400

    comment

--- infrastructure/matomo.tf
+++ infrastructure/matomo.tf
@@ -68,30 +68,30 @@ resource "aws_ecs_task_definition" "matomo" {
   }
 }
 
-resource "aws_ecs_service" "matomo" {
-  name            = "matomo"
-  cluster         = aws_ecs_cluster.foo.id
-  task_definition = aws_ecs_task_definition.mongo.arn
-  desired_count   = 3
-  iam_role        = aws_iam_role.foo.arn
-  depends_on      = [aws_iam_role_policy.foo]
+# resource "aws_ecs_service" "matomo" {
+#   name            = "matomo"
+#   cluster         = aws_ecs_cluster.foo.id
+#   task_definition = aws_ecs_task_definition.mongo.arn
+#   desired_count   = 3
+#   iam_role        = aws_iam_role.foo.arn
+#   depends_on      = [aws_iam_role_policy.foo]
 
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
-  }
+#   ordered_placement_strategy {
+#     type  = "binpack"
+#     field = "cpu"
+#   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.foo.arn
-    container_name   = "mongo"
-    container_port   = 8080
-  }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.foo.arn
+#     container_name   = "mongo"
+#     container_port   = 8080
+#   }
 
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  }
-}
+#   placement_constraints {
+#     type       = "memberOf"
+#     expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+#   }
+# }
 
 resource "aws_cloudwatch_log_group" "matomo" {
   name = var.matomo_ecs_log_group_name
