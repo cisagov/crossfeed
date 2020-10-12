0fa4382b3495e332c6abca3615f8f582cbb31012
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-10-12 07:25:14 -0400

    redirect

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -112,6 +112,9 @@ const getToUVersion = (user) => {
 app.get('/plugins/Morpheus/images/logo.svg', (req, res) =>
   res.redirect('/matomo/plugins/Morpheus/images/logo.svg?matomo')
 );
+app.get('/index.php', (req, res) =>
+  res.redirect('/matomo/index.php')
+);
 
 const matomoProxy = createProxyMiddleware({
   target: process.env.MATOMO_URL || 'http://matomo',
