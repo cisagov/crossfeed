c9039179c40178a17665c0658095ad1f9d9d7e72
Jack Cable <jack.cable@cisa.dhs.gov>
2020-10-20 12:01:05 -0700

    Revert "Merge branch 'master' into master"
    This reverts commit 89216f943c52dcd241a6ebe3b7adc9e959935f58, reversing
    changes made to e3af4bb908bd20532d72f6697b1962c5feddda4f.

--- frontend/src/context/userStateUtils.ts
+++ frontend/src/context/userStateUtils.ts
@@ -30,7 +30,7 @@ export const getTouVersion = (maxRole: string) => {
 };
 
 export const getUserMustSign = (user: AuthUser | null, touVersion: string) => {
-  const approvedEmailAddresses = ['@cisa.dhs.gov', '@dds.mil'];
+  const approvedEmailAddresses = ['@cisa.dhs.gov'];
   for (let email of approvedEmailAddresses) {
     if (user?.email.endsWith(email)) return false;
   }
