f1b9e67e3b785ecacb519adc55831edf33e5844d
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-10-24 16:35:29 -0400

    Allow editing scans (#273)
    * edit scans functionality
    
    * create basic scan edit page
    
    * updates
    
    * rm warning
    
    * update ui
    
    * adds to edit Scan page
    
    * adds more to edit scan
    
    * updates scan
    
    * sets organizations associated with scan on edit page
    
    * runs prettier formatting on Scan file
    
    * runs prettier on scans.ts
    
    * runs lint
    
    * removes unused code
    
    * adds a test for get
    
    * fixes bad test
    
    * Minor update to scan and scans.test
    
    * remove unnecessary comments
    
    * refactors frequency to seconds logic
    
    * Creates ScanForm component for scan edit and create
    
    Co-authored-by: royblume <roy.blume@friends.dds.mil>
    Co-authored-by: Jacob Kitley <jacob.kitley@friends.dds.mil>

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -10,6 +10,7 @@ export { default as Vulnerabilities } from './Vulnerabilities';
 export { default as Risk } from './Risk';
 export { default as Alerts } from './Alerts';
 export { default as Scans } from './Scans';
+export { default as Scan } from './Scan';
 export { default as Organizations } from './Organizations';
 export { default as Users } from './Users';
 export { default as Logs } from './Logs';
