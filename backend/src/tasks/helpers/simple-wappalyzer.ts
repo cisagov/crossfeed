a539a1f53082de3ef2fd3edbcf358e7d4edde073
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-10-24 16:14:31 -0400

    add more technologies

--- backend/src/tasks/helpers/simple-wappalyzer.ts
+++ backend/src/tasks/helpers/simple-wappalyzer.ts
@@ -30,6 +30,7 @@ import { JSDOM, VirtualConsole } from 'jsdom';
 import { Cookie } from 'tough-cookie';
 
 const data = require('wappalyzer/technologies.json');
+const extraTechnologies = require('./technologies.json');
 
 const parseCookie = (str) => Cookie!.parse(str)!.toJSON();
 
@@ -58,65 +59,7 @@ const getMeta = (document) =>
 
 export const technologies = {
   ...data.technologies,
-  Sitefinity: {
-    cats: [1],
-    icon: 'Sitefinity.svg',
-    cpe: 'cpe:/a:progress:sitefinity',
-    implies: 'Microsoft ASP.NET',
-    js: {
-      'Telerik.Sitefinity': ''
-    },
-    meta: {
-      generator: '^Sitefinity (\\S+)\\;version:\\1'
-    },
-    website: 'https://www.progress.com/sitefinity-cms',
-    examples: [
-      {
-        name: 'with version and suffix',
-        html:
-          '</script><meta name="Generator" content="Sitefinity 11.0.6700.0 PE" /><link',
-        version: '11.0.6700.0'
-      },
-      {
-        name: 'with version and no suffix',
-        html:
-          '</script><meta name="Generator" content="Sitefinity 11.0.6700.0" /><link',
-        version: '11.0.6700.0'
-      }
-    ]
-  },
-  'Telerik UI for ASP.NET AJAX': {
-    cats: [59],
-    cpe: 'telerik:ui_for_asp.net_ajax',
-    js: {
-      Telerik: ''
-    },
-    html: [
-      '[<link|<script].*?Telerik\\.Web\\.UI\\.WebResource\\.axd',
-      '[<link|<script].*?Telerik\\.Web\\.UI\\.WebResource\\.axd\\?.*?Telerik\\.Web\\.UI%2c\\+Version%3d(20.*?)%2c\\;version:\\1',
-      '<!-- (\\d{4}\\.\\d\\.\\d{3}\\.\\d{2}) -->\\;version:\\1'
-    ],
-    website: 'https://www.telerik.com/products/aspnet-ajax.aspx',
-    examples: [
-      {
-        name: 'with version',
-        html:
-          '<head><link href="/Telerik.Web.UI.WebResource.axd?compress=1&amp;_TSM_CombinedScripts_=%3b%3bTelerik.Web.UI%2c+Version%3d2014.2.724.40%2c+Culture%3dneutral%2c+PublicKeyToken%3d7a3d4%3aen-US%3af-7b88-4e1e-8026-2b8c288%3aed2942d4" type="text/css" rel="stylesheet" /></head>',
-        version: '2014.2.724.40'
-      },
-      {
-        name: 'with no version',
-        html:
-          '<script src="/Telerik.Web.UI.WebResource.axd?_TSM_HiddenField_=ctl02_TSM&amp;compress=0&amp;_TSM_CombinedScripts_=%3b%3bSystem.Web.Extensions%2c+Version%3d4.0.0.0%2c+Culture%3dneutral%2c+PublicKeyToken%3d7a3d4%3aen-US%3af-7b88-4e1e-8026-2b8c288%3aed2942d4" type="text/javascript"></script>',
-        version: ''
-      },
-      {
-        name: 'with version in comment',
-        html: '\t<!-- 2016.1.113.45 --><ul',
-        version: '2016.1.113.45'
-      }
-    ]
-  }
+  ...extraTechnologies
 };
 
 setTechnologies(technologies);
