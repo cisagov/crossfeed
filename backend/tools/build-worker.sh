a301bba6d6ccef088b2b4a13804508146c713dc4
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-11-01 20:25:14 -0500

    caching better

--- backend/tools/build-worker.sh
+++ backend/tools/build-worker.sh
@@ -6,8 +6,27 @@
 
 set -e
 
-WORKER_TAG=crossfeed-staging-worker
+BUILD_CACHE=/home/runner/.docker/buildkit
+DOCKER_IMAGE=crossfeed-worker
 
-./tools/pull-worker.sh $WORKER_TAG || "Failed to pull worker"
+if [ $CI = "true" ]; then
+    # From https://gist.github.com/UrsaDK/f90c9632997a70cfe2a6df2797731ac8
+    BUILDKIT_URL="$(curl -sL https://api.github.com/repos/moby/buildkit/releases \
+        | jq -r 'map(select(.name|startswith("v")))|sort_by(.name)[-1].assets[]|select(.name|endswith(".linux-amd64.tar.gz")).browser_download_url')"
+        curl -L "${BUILDKIT_URL}" | sudo tar -xz -C /usr/local
+    sudo --non-interactive --shell <<END_SUDO
+        install -d -m 0750 -o root -g docker /run/buildkit
+        buildkitd &
+        while ! test -S /run/buildkit/buildkitd.sock; do sleep 0.1; done
+        chgrp docker /run/buildkit/buildkitd.sock
+END_SUDO
 
-docker build --cache-from local.registry/$WORKER_TAG -t crossfeed-worker -f Dockerfile.worker .
+    buildctl build \
+        --frontend=dockerfile.v0 --local dockerfile=. --local context=. \
+        --export-cache type=local,dest=${BUILD_CACHE},mode=max \
+        --import-cache type=local,src=${BUILD_CACHE} \
+        --output type=docker,name=${DOCKER_IMAGE} | docker load
+    
+else 
+    docker build -t $DOCKER_IMAGE -f Dockerfile.worker .
+fi
--- backend/tools/pull-worker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Pulls staging worker.
-# This makes CI builds much faster if AWS credentials are available,
-# as layers can be cached.
-
-AWS_ECR_DOMAIN=957221700844.dkr.ecr.us-east-1.amazonaws.com
-WORKER_TAG=$1
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ECR_DOMAIN
-docker pull $AWS_ECR_DOMAIN/$WORKER_TAG:latest
