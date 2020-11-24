f733cf798faed0f779ffa726349d6c42fc917162
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-11-24 14:30:45 -0500

    fix favicon

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -149,7 +149,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/federalist-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-netlify-cms`,
