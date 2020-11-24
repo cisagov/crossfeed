b89aa58564efe5497af611fbba204b1b808d280d
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2020-11-23 22:55:54 -0500

    API ref

--- docs/src/pages/api-reference.js
+++ docs/src/pages/api-reference.js
@@ -4,11 +4,12 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
+import spec from "./swagger.json";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="API Reference" />
-    <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+    <SwaggerUI spec={spec} />
   </Layout>
 );
 
