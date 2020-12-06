97c2a58206700236d5efc122015b179f90986208
Jack Cable <jack.cable@cisa.dhs.gov>
2020-12-06 11:58:33 -0600

    Fix padding

--- frontend/src/components/Table/TableHead.tsx
+++ frontend/src/components/Table/TableHead.tsx
@@ -14,36 +14,47 @@ export const TableHead = <T extends object>({
     {headerGroups.map((headerGroup) => {
       const { key, ...rest } = headerGroup.getHeaderGroupProps();
       return (
-        <tr {...rest} key={key}>
-          {headerGroup.headers.map((column) => {
-            const { key, ...rest } = column.getHeaderProps();
-            return (
-              <th {...rest} key={key} className={classes.th}>
-                <div
-                  className="display-flex flex-justify flex-align-center flex-no-wrap text-no-wrap"
-                  {...column.getSortByToggleProps()}
-                >
-                  {column.render('Header')}
-                  {column.canSort && (
-                    <div
-                      className="margin-left-05"
-                      style={{
-                        visibility: column.isSorted ? 'visible' : 'hidden'
-                      }}
-                    >
-                      {column.isSortedDesc ? (
-                        <FaChevronDown />
-                      ) : (
-                        <FaChevronUp />
-                      )}
-                    </div>
-                  )}
-                </div>
-                <div>{column.canFilter ? column.render('Filter') : null}</div>
-              </th>
-            );
-          })}
-        </tr>
+        <React.Fragment key={key}>
+          <tr {...rest}>
+            {headerGroup.headers.map((column, index) => {
+              const { key, ...rest } = column.getHeaderProps();
+              return (
+                <th {...rest} key={key} className={classes.th}>
+                  <div
+                    className="display-flex flex-justify flex-align-center flex-no-wrap text-no-wrap"
+                    {...column.getSortByToggleProps()}
+                  >
+                    {column.render('Header')}
+                    {column.canSort && (
+                      <div
+                        className="margin-left-05"
+                        style={{
+                          visibility: column.isSorted ? 'visible' : 'hidden'
+                        }}
+                      >
+                        {column.isSortedDesc ? (
+                          <FaChevronDown />
+                        ) : (
+                          <FaChevronUp />
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </th>
+              );
+            })}
+          </tr>
+          <tr {...rest}>
+            {headerGroup.headers.map((column) => {
+              const { key, ...rest } = column.getHeaderProps();
+              return (
+                <th {...rest} key={key} className={classes.thFilter}>
+                  <div>{column.canFilter ? column.render('Filter') : null}</div>
+                </th>
+              );
+            })}
+          </tr>
+        </React.Fragment>
       );
     })}
   </thead>
