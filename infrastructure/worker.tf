4bd835af2ea66b9657630cf7175dbc3f43da43f3
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2021-01-08 15:45:39 -0500

    Delete webscraper_bucket

--- infrastructure/worker.tf
+++ infrastructure/worker.tf
@@ -279,25 +279,6 @@ resource "aws_ssm_parameter" "webscraper_s3_bucket_name" {
   }
 }
 
-resource "aws_s3_bucket" "webscraper_bucket" {
-  bucket        = var.webscraper_bucket_name
-  acl           = "private"
-  force_destroy = true # TODO: delete this bucket
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = {
-    Project = var.project
-    Stage   = var.stage
-  }
-}
-
 resource "aws_s3_bucket" "export_bucket" {
   bucket = var.export_bucket_name
   acl    = "private"
