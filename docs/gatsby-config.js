1097d261db601ba974786bf673cf70979d0ee61b
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2021-03-08 08:41:01 -0500

    update package-lock

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -162,7 +162,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `src/images/logo.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-meta-redirect`,
