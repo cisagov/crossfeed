c4867a0198ec6ada052cffb0b82400bacaa024ac
Dane Jensen <dane.jensen@associates.cisa.dhs.gov>
2021-03-17 13:29:42 -0400

    updated types again

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -14,6 +14,6 @@ export interface User {
   roles: Role[];
   dateAcceptedTerms: Date | null;
   acceptedTermsVersion: string | null;
-  lastLoggedIn: Date;
+  lastLoggedIn: Date | null;
   apiKeys: ApiKey[];
 }
