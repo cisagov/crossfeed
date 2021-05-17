244a664cab234bf2c411b4e35256717c58249f6d
aloftus23 <andylofti@gmail.com>
2021-05-17 12:08:04 -0400

    Add dns lookup

--- backend/src/tasks/rootDomainSync.ts
+++ backend/src/tasks/rootDomainSync.ts
@@ -3,6 +3,7 @@ import getRootDomains from './helpers/getRootDomains';
 import { Domain } from '../models';
 import { CommandOptions } from './ecs-client';
 import saveDomainsToDb from './helpers/saveDomainsToDb';
+import * as dns from 'dns';
 
 export const handler = async (commandOptions: CommandOptions) => {
   const { organizationId, organizationName } = commandOptions;
@@ -13,9 +14,17 @@ export const handler = async (commandOptions: CommandOptions) => {
   const domains: Domain[] = [];
   for (const rootDomain of rootDomains) {
     console.log(rootDomain);
+    let ipAddress;
+    try {
+      ipAddress = (await dns.promises.lookup(rootDomain)).address;
+    } catch (e) {
+      ipAddress = null;
+      continue;
+    }
     domains.push(
       plainToClass(Domain, {
         name: rootDomain,
+        ip: ipAddress,
         organization: { id: organizationId }
       })
     );
