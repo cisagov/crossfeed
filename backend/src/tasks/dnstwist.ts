3ccae974f9e8f6c4c0c7472e0c2905da304d3546
aloftus23 <andylofti@gmail.com>
2021-05-27 10:48:35 -0400

    changed tld file
    fixes #1137

--- backend/src/tasks/dnstwist.ts
+++ backend/src/tasks/dnstwist.ts
@@ -9,7 +9,7 @@ async function runDNSTwist(domain: Domain) {
   console.log(domain.name);
   const child = spawnSync(
     'dnstwist',
-    ['-r','-w', '--tld', './worker/common_tlds.dict', '-f', 'json', domain.name],
+    ['-w','-r', '--tld', './worker/common_tlds.dict', '-f', 'json', domain.name],
     {
       stdio: 'pipe',
       encoding: 'utf-8'
