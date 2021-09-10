40999567cbae28c6ad9951374b9225f5c2b271c7
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2021-09-09 21:43:06 -0400

    fix api red script

--- backend/src/api-red.ts
+++ backend/src/api-red.ts
@@ -2,6 +2,7 @@
 // Essentially, this entrypoint mimics API Gateway by passing down all requests
 // to the crossfeed prod api lambda function.
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import { Lambda } from 'aws-sdk';
 
@@ -19,25 +20,29 @@ interface LambdaResponse {
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use(async (req, res) => {
   // See https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
   // for input / output formats for API gateway lambda integration.
+  let body = JSON.stringify(req.body);
+  if (body === "{}") {
+    body = "";
+  }
   const input = {
-    "resource": req.path,
+    "resource": "/{any+}",
     "path": req.path,
     "httpMethod": req.method,
-    "headers": req.headers,
-    "multiValueHeaders": {
-    },
+    headers: req.headers,
+    multiValueHeaders: {},
     "queryStringParameters": req.query,
-    "multiValueQueryStringParameters": {
+    "multiValueQueryStringParameters": {},
+    "pathParameters": {
+      "any": req.path.substr(1) // Remove leading slash from path
     },
-    "requestContext": {
-    },
-    "pathParameters": null,
     "stageVariables": null,
-    "body": req.body,
+    "requestContext": {},
+    "body": body,
     "isBase64Encoded": false
   };
   const lambdaClient = new Lambda({
@@ -45,12 +50,15 @@ app.use(async (req, res) => {
   });
   const response = await lambdaClient.invoke({
     FunctionName: API_LAMBDA_FUNCTION_NAME,
-    InvocationType: 'Event',
+    InvocationType: 'RequestResponse',
     Payload: JSON.stringify(input)
   })
   .promise();
-  console.log("input", input, "response", response);
   const lambdaResponse: LambdaResponse = JSON.parse(response.Payload!.toString());
+  console.log(input.httpMethod, input.path, lambdaResponse.statusCode)
+  if (lambdaResponse.statusCode >= 400) {
+    console.error('Request failed with status code', lambdaResponse.statusCode, 'input', input, 'response', lambdaResponse);
+  }
   res.status(lambdaResponse.statusCode);
   for (const k in lambdaResponse.headers) {
     res.header(k, lambdaResponse.headers[k]);
