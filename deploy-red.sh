d2650cabc9602d05e515caf6f5a3ffe8aec06d54
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2021-09-28 19:30:52 -0400

    Update deploy-red.sh

--- deploy-red.sh
+++ deploy-red.sh
@@ -2,15 +2,17 @@ set -e
 
 # Infrastructure
 cd infrastructure
-terraform init -backend-config=prod.config -input=false && terraform fmt -recursive -check -diff && terraform validate && terraform apply -var-file=prod.tfvars
+terraform init -backend-config=prod.config -input=false
+terraform fmt -recursive -check -diff
+terraform validate
+terraform apply -var-file=prod.tfvars
 cd ..
 # Worker
 cd backend
+npm i
 npm run deploy-worker-prod
-cd ..
 # Backend
-cd backend
-npm i && npx sls deploy --stage=prod
+npx sls deploy --stage=prod
 cd ..
 # Frontend
 docker-compose -f docker-compose.red.yml build --parallel
