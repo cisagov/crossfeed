ab95cd0b9ca32e9825cd6ab5a3975e74767a9181
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2021-09-28 19:46:00 -0400

    Add scan to import dotgov domains (#1188)
    * Add scan to import dotgov domains
    
    * updates, tests
    
    * fix tests

---
+++ backend/src/tasks/dotgov.ts
@@ -0,0 +1,83 @@
+import { connectToDatabase, Organization, OrganizationTag } from '../models';
+import { CommandOptions } from './ecs-client';
+import axios from 'axios';
+import * as Papa from 'papaparse';
+
+interface ParsedRow {
+  'Domain Name': string;
+  'Domain Type': string;
+  Agency: string;
+  Organization: string;
+  City: string;
+  State: string;
+  'Security Contact Email': string;
+}
+
+// Only retrieve federal .gov domains
+export const DOTGOV_LIST_ENDPOINT =
+  'https://raw.githubusercontent.com/cisagov/dotgov-data/main/current-federal.csv';
+
+// To scan all .gov domains, use the below endpoint:
+// export const DOTGOV_LIST_ENDPOINT =
+//   'https://raw.githubusercontent.com/cisagov/dotgov-data/main/current-full.csv';
+
+export const DOTGOV_TAG_NAME = 'dotgov';
+export const DOTGOV_ORG_SUFFIX = ' (dotgov)';
+
+export const handler = async (commandOptions: CommandOptions) => {
+  const { data } = await axios.get(DOTGOV_LIST_ENDPOINT);
+  const parsedRows: ParsedRow[] = await new Promise((resolve, reject) =>
+    Papa.parse(data, {
+      header: true,
+      dynamicTyping: true,
+      complete: ({ data, errors }) =>
+        errors.length ? reject(errors) : resolve(data as ParsedRow[])
+    })
+  );
+
+  await connectToDatabase();
+
+  const orgsToRootDomainMap: {
+    [x: string]: { row: ParsedRow; rootDomains: Set<string> };
+  } = {};
+  for (const row of parsedRows) {
+    const orgName = row.Agency + DOTGOV_ORG_SUFFIX;
+    if (!orgsToRootDomainMap[orgName]) {
+      orgsToRootDomainMap[orgName] = { row, rootDomains: new Set() };
+    }
+    orgsToRootDomainMap[orgName].rootDomains.add(
+      row['Domain Name'].toLowerCase()
+    );
+  }
+
+  const tag =
+    (await OrganizationTag.findOne({ where: { name: DOTGOV_TAG_NAME } })) ||
+    (await OrganizationTag.create({
+      name: DOTGOV_TAG_NAME
+    }).save());
+
+  for (const [orgName, { row, rootDomains }] of Object.entries(
+    orgsToRootDomainMap
+  )) {
+    // Create a new organization if needed; else, create the same one.
+    console.log(orgName);
+    const organization =
+      (await Organization.findOne({
+        name: orgName
+      })) ||
+      (await Organization.create({
+        name: orgName,
+        tags: [tag],
+        ipBlocks: [],
+        isPassive: true,
+        rootDomains: []
+      }));
+    organization.tags = [tag];
+    // Replace existing rootDomains with new rootDomains.
+    organization.rootDomains = [...rootDomains];
+    await organization.save();
+    console.log('Finished org', orgName);
+  }
+
+  console.log(`dotgov done`);
+};
---
+++ backend/src/tasks/test/dotgov.test.ts
@@ -0,0 +1,217 @@
+import {
+  handler as dotgov,
+  DOTGOV_TAG_NAME,
+  DOTGOV_ORG_SUFFIX,
+  DOTGOV_LIST_ENDPOINT
+} from '../dotgov';
+import * as nock from 'nock';
+import {
+  connectToDatabase,
+  Scan,
+  Organization,
+  OrganizationTag
+} from '../../models';
+
+const HOST = 'https://raw.githubusercontent.com';
+const PATH = DOTGOV_LIST_ENDPOINT.replace(HOST, '');
+
+describe('dotgov', () => {
+  let scan;
+  beforeEach(async () => {
+    await connectToDatabase();
+    scan = await Scan.create({
+      name: 'dotgov',
+      arguments: {},
+      frequency: 999
+    }).save();
+    // Clean up any OrganizationTag / Organizations created from earlier tests
+    await OrganizationTag.delete({ name: DOTGOV_TAG_NAME });
+    await Organization.createQueryBuilder('organization')
+      .where('name like :name', { name: `%${DOTGOV_ORG_SUFFIX}%` })
+      .delete()
+      .execute();
+  });
+
+  test('basic test', async () => {
+    nock(HOST).get(PATH).reply(
+      200,
+      `Domain Name,Domain Type,Agency,Organization,City,State,Security Contact Email
+ACUS.GOV,Federal Agency - Executive,Administrative Conference of the United States,ADMINISTRATIVE CONFERENCE OF THE UNITED STATES,Washington,DC,info@acus.gov
+ACHP.GOV,Federal Agency - Executive,Advisory Council on Historic Preservation,ACHP,Washington,DC,domainsecurity@achp.gov
+PRESERVEAMERICA.GOV,Federal Agency - Executive,Advisory Council on Historic Preservation,ACHP,Washington,DC,domainsecurity@achp.gov
+ABMC.GOV,Federal Agency - Executive,American Battle Monuments Commission,American Battle Monuments Commission,Arlington,VA,itsec@abmc.gov
+ABMCSCHOLAR.GOV,Federal Agency - Executive,American Battle Monuments Commission,Office of Chief Information Officer,Arlington,VA,itsec@abmc.gov
+NATIONALMALL.GOV,Federal Agency - Executive,American Battle Monuments Commission,Office of Chief Information Officer,Arlington,VA,itsec@abmc.gov
+AMTRAKOIG.GOV,Federal Agency - Executive,AMTRAK,Amtrak Office of Inspector General,Washington,DC,(blank)
+ARC.GOV,Federal Agency - Executive,Appalachian Regional Commission,ARC,Washington,DC,(blank)
+ASC.GOV,Federal Agency - Executive,Appraisal Subcommittee,Appraisal Subcommittee,Washington,DC,(blank)
+AFRH.GOV,Federal Agency - Executive,Armed Forces Retirement Home,Armed Forces Retirement Home,Washington,DC,Stanley.Whitehead@afrh.gov
+GOLDWATERSCHOLARSHIP.GOV,Federal Agency - Executive,Barry Goldwater Scholarship and Excellence in Education Foundation,Barry Goldwater Scholarship and Excellence in Education Foundation,Alexandria,VA,goldwaterao@goldwaterscholarship.gov`
+    );
+
+    await dotgov({
+      organizationId: 'organizationId',
+      organizationName: 'organizationName',
+      scanId: scan.id,
+      scanName: 'scanName',
+      scanTaskId: 'scanTaskId'
+    });
+
+    const tag = await OrganizationTag.findOne({
+      where: { name: DOTGOV_TAG_NAME }
+    });
+    expect(tag).toBeTruthy();
+
+    const organizations = await Organization.createQueryBuilder('organization')
+      .innerJoinAndSelect('organization.tags', 'tags')
+      .where('tags.id = :tagId', { tagId: tag!.id })
+      .getMany();
+    expect(
+      organizations.map((o) => ({
+        name: o.name,
+        rootDomains: o.rootDomains
+      }))
+    ).toMatchSnapshot();
+  });
+
+  test('combines root domains together', async () => {
+    nock(HOST).get(PATH).reply(
+      200,
+      `Domain Name,Domain Type,Agency,Organization,City,State,Security Contact Email
+site1.gov,Federal Agency - Executive,Agency 1,Agency 1,Washington,DC,info@acus.gov
+site2.gov,Federal Agency - Executive,Agency 1,Agency 1,Washington,DC,domainsecurity@achp.gov`
+    );
+
+    await dotgov({
+      organizationId: 'organizationId',
+      organizationName: 'organizationName',
+      scanId: scan.id,
+      scanName: 'scanName',
+      scanTaskId: 'scanTaskId'
+    });
+
+    const tag = await OrganizationTag.findOne({
+      where: { name: DOTGOV_TAG_NAME }
+    });
+    expect(tag).toBeTruthy();
+
+    const organizations = await Organization.createQueryBuilder('organization')
+      .innerJoinAndSelect('organization.tags', 'tags')
+      .where('tags.id = :tagId', { tagId: tag!.id })
+      .getMany();
+
+    expect(organizations.length).toEqual(1);
+    expect(organizations[0].name).toEqual('Agency 1 (dotgov)');
+    expect(organizations[0].rootDomains).toEqual(['site1.gov', 'site2.gov']);
+  });
+
+  test("doesn't touch existing organizations with the same name but not with the dotgov-tag", async () => {
+    nock(HOST).get(PATH).reply(
+      200,
+      `Domain Name,Domain Type,Agency,Organization,City,State,Security Contact Email
+site1.gov,Federal Agency - Executive,Agency 1,Agency 1,Washington,DC,info@acus.gov`
+    );
+
+    const originalOrganization = await Organization.create({
+      name: 'Agency 1',
+      ipBlocks: [],
+      isPassive: true,
+      rootDomains: ['a', 'b', 'c']
+    }).save();
+    await dotgov({
+      organizationId: 'organizationId',
+      organizationName: 'organizationName',
+      scanId: scan.id,
+      scanName: 'scanName',
+      scanTaskId: 'scanTaskId'
+    });
+
+    const tag = await OrganizationTag.findOne({
+      where: { name: DOTGOV_TAG_NAME }
+    });
+    expect(tag).toBeTruthy();
+
+    const organizations = await Organization.createQueryBuilder('organization')
+      .innerJoinAndSelect('organization.tags', 'tags')
+      .where('tags.id = :tagId', { tagId: tag!.id })
+      .getMany();
+
+    expect(organizations.length).toEqual(1);
+    expect(organizations[0].name).toEqual('Agency 1 (dotgov)');
+    expect(organizations[0].rootDomains).toEqual(['site1.gov']);
+    expect(organizations[0].id).not.toEqual(originalOrganization.id);
+  });
+
+  test('should use existing tag with DOTGOV_TAG_NAME if it exists', async () => {
+    nock(HOST).get(PATH).reply(
+      200,
+      `Domain Name,Domain Type,Agency,Organization,City,State,Security Contact Email
+site1.gov,Federal Agency - Executive,Agency 1,Agency 1,Washington,DC,info@acus.gov`
+    );
+
+    const originalTag = await OrganizationTag.create({
+      name: DOTGOV_TAG_NAME
+    }).save();
+    await dotgov({
+      organizationId: 'organizationId',
+      organizationName: 'organizationName',
+      scanId: scan.id,
+      scanName: 'scanName',
+      scanTaskId: 'scanTaskId'
+    });
+
+    const tags = await OrganizationTag.find({
+      where: { name: DOTGOV_TAG_NAME }
+    });
+    expect(tags.length).toEqual(1);
+    const tag = tags[0];
+    expect(tag!.id).toEqual(originalTag.id);
+
+    const organizations = await Organization.createQueryBuilder('organization')
+      .innerJoinAndSelect('organization.tags', 'tags')
+      .where('tags.id = :tagId', { tagId: tag!.id })
+      .getMany();
+
+    expect(organizations.length).toEqual(1);
+    expect(organizations[0].name).toEqual('Agency 1 (dotgov)');
+    expect(organizations[0].rootDomains).toEqual(['site1.gov']);
+  });
+
+  test('updates existing organizations with the same name and with the dotgov-tag -- should replace all rootDomains', async () => {
+    nock(HOST).get(PATH).reply(
+      200,
+      `Domain Name,Domain Type,Agency,Organization,City,State,Security Contact Email
+site1.gov,Federal Agency - Executive,Agency 1,Agency 1,Washington,DC,info@acus.gov`
+    );
+
+    const tag = await OrganizationTag.create({
+      name: DOTGOV_TAG_NAME
+    }).save();
+
+    const originalOrganization = await Organization.create({
+      name: 'Agency 1 (dotgov)',
+      ipBlocks: [],
+      tags: [tag],
+      isPassive: false,
+      rootDomains: ['a', 'b', 'c']
+    }).save();
+    await dotgov({
+      organizationId: 'organizationId',
+      organizationName: 'organizationName',
+      scanId: scan.id,
+      scanName: 'scanName',
+      scanTaskId: 'scanTaskId'
+    });
+
+    const organizations = await Organization.createQueryBuilder('organization')
+      .innerJoinAndSelect('organization.tags', 'tags')
+      .where('tags.id = :tagId', { tagId: tag!.id })
+      .getMany();
+
+    expect(organizations.length).toEqual(1);
+    expect(organizations[0].name).toEqual('Agency 1 (dotgov)');
+    expect(organizations[0].rootDomains).toEqual(['site1.gov']);
+    expect(organizations[0].id).toEqual(originalOrganization.id);
+    expect(organizations[0].isPassive).toEqual(false);
+  });
+});
