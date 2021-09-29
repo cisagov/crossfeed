b025d6d8d12c822f1d6f4109a8d4206e9b1598e5
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2021-09-29 14:33:16 -0400

    updates

--- backend/src/tasks/pesyncdb.ts
+++ backend/src/tasks/pesyncdb.ts
@@ -19,12 +19,17 @@ export const handler: Handler = async (event) => {
     await connection.query(
       `CREATE USER ${process.env.PE_DB_USERNAME} WITH PASSWORD '${process.env.PE_DB_PASSWORD}';`
     );
+  } catch (e) {
+    console.log(
+      "Create user failed. This usually means that the user already exists, so you're OK if that was the case. Here's the exact error:", e);
+  }
+  try {
     await connection.query(
       `CREATE DATABASE ${process.env.PE_DB_NAME} owner ${process.env.PE_DB_USERNAME};`
     );
   } catch (e) {
     console.log(
-      "Create user / database failed. This means that the database already exists, so you're probably OK.", e);
+      "Create database failed. This usually means that the database already exists, so you're OK if that was the case. Here's the exact error:", e);
   }
 
   // Connect to the PE database.
