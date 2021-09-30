f62efd4d33e40c30303757baee0370e181f8ab02
aloftus23 <andylofti@gmail.com>
2021-09-30 11:22:47 -0400

    draft PR commit to sync with Dane

--- backend/src/tasks/dnstwist.ts
+++ backend/src/tasks/dnstwist.ts
@@ -4,11 +4,20 @@ import { CommandOptions } from './ecs-client';
 import { plainToClass } from 'class-transformer';
 import saveVulnerabilitiesToDb from './helpers/saveVulnerabilitiesToDb';
 import { spawnSync } from 'child_process';
+import { Client } from 'pg';
 
 async function runDNSTwist(domain: Domain) {
   const child = spawnSync(
     'dnstwist',
-    ['-r', '--tld', './worker/common_tlds.dict', '-f', 'json', domain.name],
+    [
+      '-r',
+      '-s',
+      '--tld',
+      './worker/common_tlds.dict',
+      '-f',
+      'json',
+      domain.name
+    ],
     {
       stdio: 'pipe',
       encoding: 'utf-8'
@@ -27,53 +36,70 @@ async function runDNSTwist(domain: Domain) {
 export const handler = async (commandOptions: CommandOptions) => {
   const { organizationId, organizationName } = commandOptions;
   await connectToDatabase();
+
+  // connect to the PE Database.
+  const client = new Client({
+    user: process.env.PE_DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.PE_DB_NAME,
+    password: process.env.PE_DB_PASSWORD
+  });
+  client.connect();
+  // Select the PE database org information
+  const selectQuery = `SELECT (organizations_uid,name) FROM organizations`;
+  await client.query(selectQuery, (err: any, res: any) => {
+    console.log(err, res);
+    client.end();
+  });
+
   const dateNow = new Date(Date.now());
   console.log('Running dnstwist on organization', organizationName);
   const domainsWithIPs = await getIps(organizationId);
   const vulns: Vulnerability[] = [];
-  for (const domain of domainsWithIPs) {
-    try {
-      const results = await runDNSTwist(domain);
-      // Fetch existing dnstwist vulnerability
-      const existingVuln = await Vulnerability.findOne({
-        domain: { id: domain.id },
-        source: 'dnstwist'
-      });
-      // Map date-first-observed to any domain-name that already exists
-      const existingVulnsMap = {};
-      if (existingVuln) {
-        for (const domain of existingVuln.structuredData['domains']) {
-          existingVulnsMap[domain['domain-name']] =
-            domain['date-first-observed'];
-        }
-      }
-      // If an existing domain-name is in the new results, set date-first-observed to the mapped value
-      // Else, set date-first-observed to today's date (dateNow)
-      for (const domain of results) {
-        domain['date-first-observed'] =
-          existingVulnsMap[domain['domain-name']] || dateNow;
-      }
-      if (Object.keys(results).length !== 0) {
-        vulns.push(
-          plainToClass(Vulnerability, {
-            domain: domain,
-            lastSeen: new Date(Date.now()),
-            title: 'DNS Twist Domains',
-            state: 'open',
-            source: 'dnstwist',
-            severity: 'Low',
-            needsPopulation: false,
-            structuredData: { domains: results },
-            description: `Registered domains similar to ${domain.name}.`
-          })
-        );
-        await saveVulnerabilitiesToDb(vulns, false);
-      } else {
-        continue;
-      }
-    } catch (e) {
-      console.error(e);
-      continue;
-    }
-  }
+  // for (const domain of domainsWithIPs) {
+  //   try {
+  //     const results = await runDNSTwist(domain);
+  //     // Fetch existing dnstwist vulnerability
+  //     const existingVuln = await Vulnerability.findOne({
+  //       domain: { id: domain.id },
+  //       source: 'dnstwist'
+  //     });
+  //     // Map date-first-observed to any domain-name that already exists
+  //     const existingVulnsMap = {};
+  //     if (existingVuln) {
+  //       for (const domain of existingVuln.structuredData['domains']) {
+  //         existingVulnsMap[domain['domain-name']] =
+  //           domain['date-first-observed'];
+  //       }
+  //     }
+  //     // If an existing domain-name is in the new results, set date-first-observed to the mapped value
+  //     // Else, set date-first-observed to today's date (dateNow)
+  //     for (const domain of results) {
+  //       domain['date-first-observed'] =
+  //         existingVulnsMap[domain['domain-name']] || dateNow;
+  //     }
+  //     if (Object.keys(results).length !== 0) {
+  //       vulns.push(
+  //         plainToClass(Vulnerability, {
+  //           domain: domain,
+  //           lastSeen: new Date(Date.now()),
+  //           title: 'DNS Twist Domains',
+  //           state: 'open',
+  //           source: 'dnstwist',
+  //           severity: 'Low',
+  //           needsPopulation: false,
+  //           structuredData: { domains: results },
+  //           description: `Registered domains similar to ${domain.name}.`
+  //         })
+  //       );
+  //       await saveVulnerabilitiesToDb(vulns, false);
+  //       console.log(results);
+  //     } else {
+  //       continue;
+  //     }
+  //   } catch (e) {
+  //     console.error(e);
+  //     continue;
+  //   }
+  // }
 };
