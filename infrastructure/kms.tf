7ae8d8ecb45279a3685a391d7d9b18fcc1f1d1af
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2021-11-29 14:45:32 -0500

    Fix kms permissions for terraform (#1335)

--- infrastructure/kms.tf
+++ infrastructure/kms.tf
@@ -2,4 +2,40 @@ resource "aws_kms_key" "key" {
   description             = "KMS key"
   deletion_window_in_days = 10
   enable_key_rotation     = true
+  policy                  = <<EOF
+{
+    "Version": "2012-10-17",
+    "Id": "key-default-1",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+            },
+            "Action": "kms:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "logs.${data.aws_region.current.name}.amazonaws.com"
+            },
+            "Action": [
+                "kms:Encrypt*",
+                "kms:Decrypt*",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:Describe*"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "ArnLike": {
+                    "kms:EncryptionContext:aws:logs:arn": "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+                }
+            }
+        } 
+    ]
+}
+  EOF
 }
