1d38cac7b50451cabfd905fa7a97604a7b9cbcba
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2021-11-30 10:57:43 -0500

    Remove ssh2 ignore, fixes https://github.com/cisagov/crossfeed/issues/1345

--- backend/webpack.backend.config.js
+++ backend/webpack.backend.config.js
@@ -24,7 +24,6 @@ module.exports = {
   plugins: [
     new webpack.IgnorePlugin(/^pg-native$/),
     new webpack.IgnorePlugin(/^canvas$/), // imported by jsdom from simple-wapplyzer, not used so we can ignore.
-    new webpack.IgnorePlugin(/^ssh2$/) // imported by dockerode, doesn't compile properly when deployed to AWS lambda, not used in production (only used in local development) so we can ignore.
   ],
   resolve: {
     modules: ['node_modules', 'scripts'],
