147eecceafdf9e5c33c7a6ed2da88f48060af06c
Your Name <you@example.com>
2022-03-10 18:38:31 +0000

    Latest updates in the cool before accessor

--- backend/worker/pe_scripts/sync_dnstwist_pe.py
+++ backend/worker/pe_scripts/sync_dnstwist_pe.py
@@ -3,6 +3,7 @@ import traceback
 import psycopg2
 import psycopg2.extras as extras
 import requests
+import socket
 
 
 def query_db(conn, query, args=(), one=False):
@@ -14,6 +15,67 @@ def query_db(conn, query, args=(), one=False):
     ]
 
     return (r[0] if r else None) if one else r
+
+
+def getSubdomain(conn, domain):
+    cur = conn.cursor()
+    sql = f"""SELECT * FROM sub_domains sd
+        WHERE sd.sub_domain = '{domain}'"""
+    cur.execute(sql)
+    sub = cur.fetchone()
+    cur.close()
+    return sub
+
+
+def getRootdomain(conn, domain):
+    cur = conn.cursor()
+    sql = f"""SELECT * FROM root_domains rd
+        WHERE rd.root_domain = '{domain}'"""
+    cur.execute(sql)
+    root = cur.fetchone()
+    cur.close()
+    return root
+
+
+def addRootdomain(conn, root_domain, pe_org_uid, source_uid, org_name):
+    ip_address = str(socket.gethostbyname(root_domain))
+    sql = f"""insert into root_domains(root_domain, organizations_uid, organization_name, data_source_uid, ip_address)
+            values ('{root_domain}', '{pe_org_uid}', '{org_name}', '{source_uid}', '{ip_address});"""
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    cur.close()
+    print(f"Success adding root domain, {root_domain}, to root domain table.")
+
+
+def addSubdomain(conn, domain, pe_org_uid, org_name):
+    source_uid = getDataSource(conn, "findomain")[0]
+    root_domain = domain.split(".")[-2:]
+    root_domain = ".".join(root_domain)
+    print(root_domain)
+    try:
+        root_domain_uid = getRootdomain(conn, root_domain)[0]
+        print(root_domain_uid)
+    except:
+        addRootdomain(conn, domain, pe_org_uid, source_uid, org_name)
+        root_domain_uid = getRootdomain(conn, root_domain)[0]
+    sql = f"""insert into sub_domains(sub_domain, root_domain_uid, root_domain, data_source_uid)
+            values ('{domain}', '{root_domain_uid}', '{root_domain}', '{source_uid}');"""
+    print(sql)
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    cur.close()
+    print(f"Success adding domain, {domain}, to subdomains table.")
+
+
+def getDataSource(conn, source):
+    cur = conn.cursor()
+    sql = f"""SELECT * FROM data_source WHERE name='{source}'"""
+    cur.execute(sql)
+    source = cur.fetchone()
+    cur.close()
+    return source
 
 
 DB_HOST = os.environ.get("DB_HOST")
@@ -64,7 +126,7 @@ except:
 
 """Collect DNSTwist data from Crossfeed"""
 try:
-    sql = f"""SELECT vuln."structuredData", vuln."domainId"
+    sql = f"""SELECT vuln."structuredData", vuln."domainId", dom."name"
                 FROM domain as dom
                 JOIN vulnerability as vuln
                 ON vuln."domainId" = dom.id
@@ -72,10 +134,23 @@ try:
                 AND vuln."source" = 'dnstwist'"""
     dnstwist_resp = query_db(CF_conn, sql)
 
+    # Get data source
+    source_uid = getDataSource(PE_conn, "DNSTwist")[0]
+
     domain_list = []
     perm_list = []
     for row in dnstwist_resp:
+        # Get subdomain uid
+        sub_domain = row["name"]
+        print(sub_domain)
         row = row["structuredData"]["domains"]
+        try:
+            sub_domain_uid = getSubdomain(PE_conn, sub_domain)[0]
+        except:
+            # Add and then get it
+            addSubdomain(PE_conn, sub_domain, pe_org_uid, org_name)
+            sub_domain_uid = getSubdomain(PE_conn, sub_domain)[0]
+
         for dom in row:
             malicious = False
             attacks = 0
@@ -121,6 +196,8 @@ try:
 
             domain_dict = {
                 "organizations_uid": pe_org_uid,
+                "data_source_uid": source_uid,
+                "sub_domain_uid": sub_domain_uid,
                 "domain_permutation": dom["domain-name"],
                 "ipv4": dom["dns-a"][0],
                 "ipv6": dom["dns-aaaa"][0],
@@ -134,6 +211,7 @@ try:
                 "blocklist_report_count": reports,
             }
             domain_list.append(domain_dict)
+        print(perm_list)
 
 except:
     print("Failed selecting DNSTwist data.")
@@ -145,12 +223,13 @@ CF_conn.close()
 try:
     cursor = PE_conn.cursor()
     columns = domain_list[0].keys()
-    table = "dnstwist_domain_masq"
+    table = "domain_permutations"
     sql = """INSERT INTO %s(%s) VALUES %%s 
-    ON CONFLICT (domain_permutation) 
+    ON CONFLICT (domain_permutation,organizations_uid) 
     DO UPDATE SET malicious = EXCLUDED.malicious,
         blocklist_attack_count = EXCLUDED.blocklist_attack_count,
-        blocklist_report_count = EXCLUDED.blocklist_report_count;""" % (
+        blocklist_report_count = EXCLUDED.blocklist_report_count,
+        data_source_uid = EXCLUDED.data_source_uid;""" % (
         table,
         ",".join(columns),
     )
