01b721adcfd3e945a0dbd8668eaa1973ac6afe28
aloftus23 <andylofti@gmail.com>
2022-04-05 14:20:42 -0400

    remove source from aws provider

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -4,16 +4,15 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
-  shared_credentials_files = ["$HOME/.aws/credentials"]
   alias                    = "virginia"
   region                   = "us-east-1"
+  shared_credentials_files = ["$HOME/.aws/credentials"]
 }
 
 terraform {
