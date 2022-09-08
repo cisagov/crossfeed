1db957cb7b0bc385c61a1ccb1be9ccfa6650e5f6
aloftus23 <andylofti@gmail.com>
2022-09-08 09:46:15 -0400

    Add swagger definititons

--- backend/src/api/pe-reports.ts
+++ backend/src/api/pe-reports.ts
@@ -2,6 +2,15 @@ import { wrapHandler } from './helpers';
 import S3Client from '../tasks/s3-client';
 import { getOrgMemberships } from './auth';
 
+/**
+ * @swagger
+ *
+ * /pe-reports/export:
+ *  get:
+ *    description: Export P&E report by specifying the S3 key returned in /pe-reports/list.
+ *    tags:
+ *    - Reports
+ */
 export const export_pe_report = wrapHandler(async (event) => {
   const Key = JSON.parse(event.body!).Key;
   const client = new S3Client();
@@ -18,9 +27,20 @@ export const export_pe_report = wrapHandler(async (event) => {
   };
 });
 
+/**
+ * @swagger
+ *
+ * /pe-reports/list:
+ *  get:
+ *    description: Get a list of available P&E reports by specifying organization id. User must be a member of the organization.
+ *    tags:
+ *    - Reports
+ */
 export const list_pe_reports = wrapHandler(async (event) => {
   const orgId = JSON.parse(event.body!).currentOrganization.id;
   const orgName = JSON.parse(event.body!).currentOrganization.name;
+  console.log(orgName);
+  console.log(getOrgMemberships(event));
   if (getOrgMemberships(event).includes(orgId)) {
     const client = new S3Client();
     const data = await client.listPeReports(orgName);
