a76dcfa43b5651426b77419a9ca9fb6c1ace2404
bgtripp <bentrippx@gmail.com>
2022-09-14 15:22:03 -0700

    Fix docstring in run-autodisable.ts

--- backend/src/tools/run-autodisable.ts
+++ backend/src/tools/run-autodisable.ts
@@ -1,11 +1,7 @@
 import { handler as autodisable } from '../tasks/autoDisableAccounts';
 
 /**
- * Runs syncdb locally. This script is called from running "npm run syncdb".
- * Call "npm run syncdb -- -d dangerouslyforce" to force dropping and
- * recreating the SQL database.
- * Call "npm run syncdb -- -d populate" to populate the database
- * with some sample data.
+ * Runs autodisable locally. This script is called from running "npm run autodisable".
  * */
 
 process.env.DB_HOST = 'db';
