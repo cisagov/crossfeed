901ab5b09798d4953102c081eaaa074becd240e9
bgtripp <bentrippx@gmail.com>
2022-09-14 15:16:07 -0700

    Create test suite for autodisable handler

---
+++ backend/test/autodisable.test.ts
@@ -0,0 +1,57 @@
+import { User, connectToDatabase } from '../src/models';
+import { handler as autodisable } from '../src/tasks/autoDisableAccounts';
+
+describe('auto disable', () => {
+  beforeAll(async () => {
+    await connectToDatabase();
+  });
+  const INACTIVE_THRESHOLD = 60;
+  it(`auto disable should disable accounts that last logged in over ${INACTIVE_THRESHOLD} days ago`, async () => {
+    const days_over_thresh = [0, 1, 50, 200, 365];
+    var users: User[] = [];
+    for (let i = 0; i < days_over_thresh.length; i++) {
+      var inactiveDate = new Date();
+      inactiveDate.setDate(
+        inactiveDate.getDate() - INACTIVE_THRESHOLD - days_over_thresh[i]
+      );
+      let user = await User.create({
+        firstName: Math.random().toString(),
+        lastName: '',
+        email: Math.random() + '@crossfeed.cisa.gov',
+        dateAcceptedTerms: new Date('2020-08-03T13:58:31.715Z'),
+        lastLoggedIn: inactiveDate
+      }).save();
+      users.push(user);
+    }
+    let response = await autodisable('', {} as any, () => null);
+    for (let i = 0; i < users.length; i++) {
+      const retrievedUser = await User.findOneOrFail({
+        email: users[i].email
+      });
+      expect(retrievedUser.disabled).toEqual(true);
+    }
+  });
+  it(`auto disable should disable accounts that last logged in under ${INACTIVE_THRESHOLD} days ago`, async () => {
+    const days_back = [0, 1, 15, 30, 59];
+    var users: User[] = [];
+    for (let i = 0; i < days_back.length; i++) {
+      var inactiveDate = new Date();
+      inactiveDate.setDate(inactiveDate.getDate() - days_back[i]);
+      let user = await User.create({
+        firstName: Math.random().toString(),
+        lastName: '',
+        email: Math.random() + '@crossfeed.cisa.gov',
+        dateAcceptedTerms: new Date('2020-08-03T13:58:31.715Z'),
+        lastLoggedIn: inactiveDate
+      }).save();
+      users.push(user);
+    }
+    let response = await autodisable('', {} as any, () => null);
+    for (let i = 0; i < users.length; i++) {
+      const retrievedUser = await User.findOneOrFail({
+        email: users[i].email
+      });
+      expect(retrievedUser.disabled).toEqual(false);
+    }
+  });
+});
