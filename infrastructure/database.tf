29196df8a763b3f2f85705044cddfef54d3a8e7b
aloftus23 <andylofti@gmail.com>
2022-11-10 12:38:29 -0500

    Move S3 resources to database.tf

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -197,3 +197,72 @@ resource "aws_ssm_parameter" "crossfeed_send_db_name" {
     Project = var.project
   }
 }
+
+resource "aws_s3_bucket" "pe_reports_bucket" {
+  bucket = var.pe_reports_bucket_name
+  tags = {
+    Project = var.project
+    Stage   = var.stage
+  }
+}
+
+resource "aws_s3_bucket_acl" "pe_reports_bucket" {
+  bucket = aws_s3_bucket.pe_reports_bucket.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "pe_reports_bucket" {
+  bucket = aws_s3_bucket.pe_reports_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "pe_reports_bucket" {
+  bucket = aws_s3_bucket.pe_reports_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_logging" "pe_reports_bucket" {
+  bucket        = aws_s3_bucket.pe_reports_bucket.id
+  target_bucket = aws_s3_bucket.pe_reports_bucket.id
+  target_prefix = "pe_reports_bucket/"
+}
+
+resource "aws_s3_bucket" "pe_db_backups_bucket" {
+  bucket = var.db_backups_bucket_name
+  tags = {
+    Project = var.project
+    Stage   = var.stage
+  }
+}
+
+resource "aws_s3_bucket_acl" "pe_db_backups_bucket" {
+  bucket = aws_s3_bucket.pe_db_backups_bucket.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "pe_db_backups_bucket" {
+  bucket = aws_s3_bucket.pe_db_backups_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "pe_db_backups_bucket" {
+  bucket = aws_s3_bucket.pe_db_backups_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_logging" "pe_db_backups_bucket" {
+  bucket        = aws_s3_bucket.pe_db_backups_bucket.id
+  target_bucket = aws_s3_bucket.pe_db_backups_bucket.id
+  target_prefix = "pe_db_backups_bucket/"
+}
+
