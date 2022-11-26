a023dfd888658f3a62695cae29b288e9f85b5327
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-11-26 15:44:10 +0000

    revert docs

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -159,5 +159,8 @@ module.exports = {
       },
     },
     `gatsby-plugin-meta-redirect`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 };
