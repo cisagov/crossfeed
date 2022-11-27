05d315979a5349b33255c6371bb795c3601727bb
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-11-27 00:49:32 +0000

    done

--- frontend/src/pages/AuthLogin/AuthLogin.tsx
+++ frontend/src/pages/AuthLogin/AuthLogin.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthForm } from 'components';
 import { Button } from '@trussworks/react-uswds';
