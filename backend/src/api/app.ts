e7ebb4db000f34e437895be2e4250f379b950176
aloftus23 <79927030+aloftus23@users.noreply.github.com>
2023-01-19 15:01:59 -0500

    Update backend/src/api/app.ts
    Co-authored-by: Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -322,7 +322,7 @@ authenticatedRoute.post(
   handlerToExpress(reports.export_report)
 );
 
-authenticatedRoute.post(
+authenticatedRoute.get(
   '/reports/list',
   handlerToExpress(reports.list_reports)
 =======
