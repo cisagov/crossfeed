d2e76a88f967760c4d560109aacde8cd9b7a92aa
rc-raizen <rexcazaubon340@gmail.com>
2023-02-13 17:28:45 -0400

    testing notifications.ts orgs and lists

--- backend/src/tasks/test/notifications.test.ts
+++ backend/src/tasks/test/notifications.test.ts
@@ -1,28 +1,51 @@
 import { handler as notifications} from '../notifications';
 import app from '../../api/app';
 import * as request from 'supertest';
-import { connectToDatabase, Organization, OrganizationTag } from '../../models';
+import { connectToDatabase, Organization, Role, User } from '../../models';
 
 jest.mock('../s3-client');
 import { createUserToken } from '../../../../backend/test/util';
 const latestReports = require('../s3-client').listReports as jest.Mock;
 
 describe ('notifications', () => {
-    let organization;
+    
     beforeAll(async () => {
         await connectToDatabase();
     });
-    test('should return the list of reports', async () => {
-        const response = await request(app)
-        .get('/reports/list')
-        .set(
-          'Authorization',
-          createUserToken({
-            roles: [{ org: organization.id, role: 'user' }]
-          })
-        )
-        .send({ currentOrganization: { id: organization.id } })
+    test('list all organizations', async () => {
+        const organization = await Organization.create({
+            name: 'test-' + Math.random(),
+            rootDomains: ['test-' + Math.random()],
+            ipBlocks: [],
+            isPassive: false
+          }).save();
         
-        expect(latestReports).toReturn;
+          
+        const response = await request(app)
+            .get('/organizations')
+            .set(
+              'Authorization',
+             createUserToken({
+               roles: [{ org: organization.id, role: 'user' }]
+             })
+            )
+            .send({ currentOrganization: { id: organization.id} })
+            .expect(200);
+            
+            await notifications(
+                [organization.id],
+                {} as any,
+                () => void 0
+    
+              );
+
+    });
+    test('calling reports list for all organizations', async () => {
+        const organization = await Organization.create({
+            name: 'test-' + Math.random(),
+            rootDomains: ['test-' + Math.random()],
+            ipBlocks: [],
+            isPassive: false
+          }).save();
     })
-})
+});
