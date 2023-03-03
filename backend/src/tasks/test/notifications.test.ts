819b2af011eb5e3f4d0e1acfa7d01f3e28b3af7d
rc-raizen <rexcazaubon340@gmail.com>
2023-03-03 10:32:21 -0400

    restructured made organizations for testing

--- backend/src/tasks/test/notifications.test.ts
+++ backend/src/tasks/test/notifications.test.ts
@@ -8,37 +8,34 @@ jest.mock('../s3-client');
 const listReports = require('../s3-client').listReports as jest.Mock;
 
 describe ('notifications', () => {
-   
+   let organization;
+   let organization2;
+   let organization3;
     beforeAll(async () => {
         await connectToDatabase();
+        organization = await Organization.create({
+          name: 'WAS-' + Math.random(),
+          rootDomains: ['WAS-' + Math.random()],
+          ipBlocks: [],
+          isPassive: false
+        }).save();
+       
+        organization2 = await Organization.create({
+          name: 'P&E-' + Math.random(),
+          rootDomains: ['P&E-' + Math.random()],
+          ipBlocks: [],
+          isPassive: false
+        }).save();
+        
+        organization3 = await Organization.create({
+          name: 'VS-' + Math.random(),
+          rootDomains: ['VS-' + Math.random()],
+          ipBlocks: [],
+          isPassive: false
+        }).save();
     });
     test('list all organizations', async () => {
-        await Organization.create({
-            name: 'WAS-' + Math.random(),
-            rootDomains: ['WAS-' + Math.random()],
-            ipBlocks: [],
-            isPassive: false
-          }).save();
-
-        await Organization.create({
-            name: 'P&E-' + Math.random(),
-            rootDomains: ['P&E-' + Math.random()],
-            ipBlocks: [],
-            isPassive: false
-          }).save();
-          
-        await Organization.create({
-            name: 'VS-' + Math.random(),
-            rootDomains: ['VS-' + Math.random()],
-            ipBlocks: [],
-            isPassive: false
-          }).save();
-          
-        await notifications(
-            notifications,
-            {} as any,
-            () => void 0
-            )
+       
         const response = await request(app)
             .get(`/organizations`)
             .set(
@@ -51,30 +48,43 @@ describe ('notifications', () => {
           expect(response.body.length).toBe(3); 
     });
     
-    /** test('Check if user is Global Admin', async () => {
-        const organization = await Organization.create({
-            name: 'WAS-' + Math.random(),
-            rootDomains: ['WAS-' + Math.random()],
-            ipBlocks: [],
-            isPassive: false
-          }).save();
-
-        const firstName = ' first name';
-        const lastName = 'last name';
-        const email = Math.random() + '@crossfeed.cisa.gov';
+     test('Get all org admins only', async () => {
+        
         const user = await User.create ({
-            firstName,
-            lastName,
-            email,
-            userType: UserType.GLOBAL_ADMIN
+            firstName: 'Deku',
+            lastName: '',
+            email: Math.random() + '@crossfeed.cisa.gov',
+            
         }).save();
+        const user2 = await User.create ({
+          firstName: 'Kacchan',
+          lastName: '',
+          email: Math.random() + '@crossfeed.cisa.gov',
+      }).save();
+      const user3 = await User.create ({
+        firstName: 'Todoroki',
+        lastName: '',
+        email: Math.random() + '@crossfeed.cisa.gov',
+    }).save();
         await Role.create({
             role: 'admin',
             approved: false,
-            organization,
-            user
-
+            organization: organization,
+            user: user
         }).save();
+        await Role.create({
+          role: 'admin',
+          approved: false,
+          organization: organization2,
+          user: user2
+      }).save();
+      await Role.create({
+        role: 'admin',
+        approved: false,
+        organization: organization3,
+        user: user3,
+    }).save();
+
         
         const response = await request(app)
           .get(`/organizations`)  
@@ -82,36 +92,33 @@ describe ('notifications', () => {
             'Authorization',
             createUserToken({
               roles: [{org: organization.id, role: 'admin'}],
-              userType: UserType.GLOBAL_ADMIN
+              
             })
           )
           .send({currentOrganization: {id: organization.id}})
           .expect(200)
         expect(response)
 
-    }) */
+    }) 
     
     test('getting reports list for all organizations', async () => {
-        const organization = await Organization.create({
-            name: 'test-' + Math.random(),
-            rootDomains: ['test-' + Math.random()],
-            ipBlocks: [],
-            isPassive: false
-          }).save();
-        const firstName = 'first name';
-        const lastName = 'last name';
-        const email = Math.random() + '@crossfeed.cisa.gov';
-        const user = await User.create({
-          firstName,
-          lastName,
-          email
-        }).save();
-        await Role.create({
-          role: 'user',
-          approved: false,
-          organization,
-          user
-        }).save();
+      const user = await User.create ({
+        firstName: 'Dabi',
+        lastName: '',
+        email: Math.random() + '@crossfeed.cisa.gov', 
+    }).save();
+    await Role.create({
+      role: 'user',
+      approved: false,
+      organization,
+      user
+    }).save();
+        await notifications(
+          {},
+          {} as any,
+          () => void 0
+          )
+
         const response = await request(app)
           .get('/reports/list')
           .set(
@@ -123,7 +130,7 @@ describe ('notifications', () => {
           .send({ currentOrganization: { id: organization.id } })
           .expect(200);
         expect(response.text).toEqual('{"Contents":"report content"}');
-        expect(listReports).toBeCalledTimes(1);
+        expect(listReports).toBeCalled;
     
     }) 
 });
