991f720b0f0cf93eb01d50c8d0f2807bdc8c87ae
rc-raizen <rexcazaubon340@gmail.com>
2023-03-03 10:32:00 -0400

    pulling org admins from roles model

--- backend/src/tasks/notifications.ts
+++ backend/src/tasks/notifications.ts
@@ -1,11 +1,17 @@
 import { Handler } from 'aws-lambda';
-import { connectToDatabase, Organization, User, Role, UserType } from '../models';
-import {sendEmail} from '../api/helpers';
-import S3Client from './s3-client'
-import {isOrgAdmin} from '../api/auth'
+import {
+  connectToDatabase,
+  Organization,
+  User,
+  Role,
+  UserType
+} from '../models';
+import { sendEmail } from '../api/helpers';
+import S3Client from './s3-client';
+import { isOrgAdmin } from '../api/auth';
+import { Organizations } from 'aws-sdk';
 
 const sendReportEmail = async (email: string, organization?: Organization) => {
-  
   await sendEmail(
     email,
     'New Report Available',
@@ -13,9 +19,7 @@ const sendReportEmail = async (email: string, organization?: Organization) => {
 
 The latest report for ${
       organization?.name ? `is now available on ` : ''
-    }Crossfeed. To view the report, sign on at ${
-      process.env.FRONTEND_DOMAIN
-    }
+    }Crossfeed. To view the report, sign on at ${process.env.FRONTEND_DOMAIN}
 
 Crossfeed access instructions:
 
@@ -28,64 +32,48 @@ If you encounter any difficulties, please feel free to reply to this email (or s
 };
 
 export const handler: Handler = async (event) => {
-    await connectToDatabase();
-    console.log('Running notifications check...')
-    
-    // created to test if admin can be found
-    const firstName = ' first name';
-    const lastName = 'last name';
-    const email = Math.random() + '@crossfeed.cisa.gov'; 
-     await User.create ({
-      firstName,
-      lastName,
-      email,
-      userType: UserType.GLOBAL_ADMIN
-      }).save();  
-    
-   // Get all organizations
-    const organizations = await Organization.find()
-      
-     for (const org in organizations) {
-       const client = new S3Client();
-       // pull reports list 
-       const reportsList = await client.listReports(organizations[org].id)
-       //find all global admin users only
-       const users = await User.find({
-        userType: UserType.GLOBAL_ADMIN
-         })
-         // loop through reports and send email notification
-       for (const report in reportsList) {
+  await connectToDatabase();
+  console.log('Running notifications check...');
+  
+  // Get all organizations
+  const organizations = await Organization.find();
+
+  for (const org in organizations) {
+    const client = new S3Client();
+    // pull reports list
+    const reportsList = await client.listReports(organizations[org].id);
+    // find org admin roles for each organization
+    const roles = await Role.find(
+      {
+        relations: ['user', 'organization'],
+
+        where: {
+          role: 'admin',
+          organization: organizations[org],
+        },
+      }
+    );
+
+    // loop through reports 
+    for (const report in reportsList) {
+      /** notifiedAt code goes here...
+       * if (report notifiedAt)
+      */
+
+      for (const role in roles) {
 
       // email reporting -- TODO need actual org name thats in database here
-        if (organizations[org].name.startsWith("WAS") ){
-          for ( const user in users) {
-            // TODO - check for proper use of isOrgAdmin, testing ?
-            if (isOrgAdmin(event)) {
-              await sendReportEmail('123@crossfeed.cisa.gov', organizations[org])
-            }
-          }
-        // P&E 
-         } else if (organizations[org].name.startsWith("P&E")){
-          for ( const user in users) {
-            if (isOrgAdmin(event)) {
-              await sendReportEmail(users[user].email, organizations[org])
-            }
-          }
+      if (roles[role].organization.name.startsWith('WAS')) {
+            // TODO - pulling orgadmin email to send emails
+            await sendReportEmail(roles[role].user.email, roles[role].organization);                               
+        // P&E
+      } else if (roles[role].organization.name.startsWith('P&E')) {
+            await sendReportEmail(roles[role].user.email, roles[role].organization);  
         // VS
-         } else if (organizations[org].name.startsWith("VS")){
-          for ( const user in users) {
-            if (isOrgAdmin(event)) {
-              await sendReportEmail(users[user].email, organizations[org])
-            }
-          }
-         }
-      }
-       
-     }
-    
-  
-
-  };
-  
-
-  
+      } else if (roles[role].organization.name.startsWith('VS')) {
+            await sendReportEmail(roles[role].user.email, roles[role].organization);
+    }
+    }
+  }
+  }
+};
