079ed984623e790b26a81019607eeb7ba6412244
rc-raizen <rexcazaubon340@gmail.com>
2023-03-09 11:24:08 -0400

    updated sendReport

--- backend/src/tasks/notifications.ts
+++ backend/src/tasks/notifications.ts
@@ -1,19 +1,31 @@
 import { Handler } from 'aws-lambda';
-import { S3 } from 'aws-sdk';
-import { list_reports } from 'src/api/reports';
+import { sendEmail } from '../api/helpers';
 import {
   connectToDatabase,
   Organization,
-  User,
   Role,
-  UserType
 } from '../models';
 import S3Client from './s3-client';
 import s3Client from './__mocks__/s3-client';
 
 const sendReportEmail = async (email: string, organization?: Organization) => {
-  //console.log("Email: " + email, "Organization: " + organization?.name)
-};
+  await sendEmail(
+    email,
+    'New Report Available',
+    `Hi there,
+
+The latest report for ${
+      organization?.name ? `is now available on ` : ''
+    }Crossfeed. To view the report, sign on at ${process.env.FRONTEND_DOMAIN}
+
+Crossfeed access instructions:
+
+For more information on using Crossfeed, view the Crossfeed user guide at https://docs.crossfeed.cyber.dhs.gov/user-guide/quickstart/. 
+
+If you encounter any difficulties, please feel free to reply to this email (or send an email to ${
+      process.env.CROSSFEED_SUPPORT_EMAIL_REPLYTO
+    }).`
+  );};
 
 export const handler: Handler = async (event) => {
   await connectToDatabase();
@@ -41,9 +53,9 @@ export const handler: Handler = async (event) => {
 
     // loop through reports 
     for (const report in reportsList) {
-      console.log(list)
 
       if (reportsList[report].LastModified >= organizations[org].notifiedAt){
+
       for (const role in roles) {
     
       // email reporting -- TODO need actual org name thats in database here
