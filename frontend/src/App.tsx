f0c4a5e325ae720f3b9869808c2c6afa11cd5ec2
Ashwin Ramaswami <ashwin.ramaswami@cisa.dhs.gov>
2023-03-14 22:21:52 -0500

    Fix dev issues with frontend by disabling matomo (#1740)
    * Fixes for local dev
    
    * Fix
    
    * update
    
    * fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  useLocation,
   Redirect
 } from 'react-router-dom';
 import { API, Auth } from 'aws-amplify';
 import { AuthContextProvider, CFThemeProvider, SearchProvider } from 'context';
-import {
-  MatomoProvider,
-  createInstance,
-  useMatomo
-} from '@datapunt/matomo-tracker-react';
+// import {
+//   MatomoProvider,
+//   createInstance,
+//   useMatomo
+// } from '@datapunt/matomo-tracker-react';
 import {
   Domain,
   AuthLogin,
@@ -54,113 +53,111 @@ if (process.env.REACT_APP_USE_COGNITO) {
   });
 }
 
-const instance = createInstance({
-  urlBase: `${process.env.REACT_APP_API_URL}/matomo`,
-  siteId: 1,
-  disabled: false,
-  heartBeat: {
-    // optional, enabled by default
-    active: true, // optional, default value: true
-    seconds: 15 // optional, default value: `15
-  },
-  linkTracking: false // optional, default value: true
-  // configurations: { // optional, default value: {}
-  //   // any valid matomo configuration, all below are optional
-  //   disableCookies: true,
-  //   setSecureCookie: true,
-  //   setRequestMethod: 'POST'
-  // }
-});
+// const instance = createInstance({
+//   urlBase: `${process.env.REACT_APP_API_URL}/matomo`,
+//   siteId: 1,
+//   disabled: false,
+//   heartBeat: {
+//     // optional, enabled by default
+//     active: true, // optional, default value: true
+//     seconds: 15 // optional, default value: `15
+//   },
+//   linkTracking: false // optional, default value: true
+//   // configurations: { // optional, default value: {}
+//   //   // any valid matomo configuration, all below are optional
+//   //   disableCookies: true,
+//   //   setSecureCookie: true,
+//   //   setRequestMethod: 'POST'
+//   // }
+// });
 
 const LinkTracker = () => {
-  const location = useLocation();
-  const { trackPageView } = useMatomo();
+  // const location = useLocation();
+  // const { trackPageView } = useMatomo();
 
-  useEffect(() => trackPageView({}), [location, trackPageView]);
+  // useEffect(() => trackPageView({}), [location, trackPageView]);
 
   return null;
 };
 
 const App: React.FC = () => (
-  <MatomoProvider value={instance}>
-    <Router>
-      <CFThemeProvider>
-        <AuthContextProvider>
-          <Authenticator.Provider>
-            <SearchProvider>
-              <Layout>
-                <LinkTracker />
-                <Switch>
-                  <RouteGuard
-                    exact
-                    path="/"
-                    render={() => <Redirect to="/inventory" />}
-                    unauth={AuthLogin}
-                    component={Risk}
-                  />
-                  <RouteGuard
-                    exact
-                    path="/signup"
-                    render={() => <Redirect to="/inventory" />}
-                    unauth={(props) => (
-                      <AuthLogin {...props} showSignUp={true} />
-                    )}
-                    component={Risk}
-                  />
-                  <Route
-                    exact
-                    path="/login-gov-callback"
-                    component={LoginGovCallback}
-                  />
-                  <Route
-                    exact
-                    path="/create-account"
-                    component={AuthCreateAccount}
-                  />
-                  <Route exact path="/terms" component={TermsOfUse} />
+  // <MatomoProvider value={instance}>
+  <Router>
+    <CFThemeProvider>
+      <AuthContextProvider>
+        <Authenticator.Provider>
+          <SearchProvider>
+            <Layout>
+              <LinkTracker />
+              <Switch>
+                <RouteGuard
+                  exact
+                  path="/"
+                  render={() => <Redirect to="/inventory" />}
+                  unauth={AuthLogin}
+                  component={Risk}
+                />
+                <RouteGuard
+                  exact
+                  path="/signup"
+                  render={() => <Redirect to="/inventory" />}
+                  unauth={(props) => <AuthLogin {...props} showSignUp={true} />}
+                  component={Risk}
+                />
+                <Route
+                  exact
+                  path="/login-gov-callback"
+                  component={LoginGovCallback}
+                />
+                <Route
+                  exact
+                  path="/create-account"
+                  component={AuthCreateAccount}
+                />
+                <Route exact path="/terms" component={TermsOfUse} />
 
-                  <RouteGuard exact path="/inventory" component={SearchPage} />
-                  <RouteGuard
-                    path="/inventory/domain/:domainId"
-                    component={Domain}
-                  />
-                  <RouteGuard path="/inventory/domains" component={Domains} />
-                  <RouteGuard
-                    path="/inventory/vulnerabilities"
-                    exact
-                    component={Vulnerabilities}
-                  />
-                  <RouteGuard
-                    path="/inventory/vulnerabilities/grouped"
-                    component={(props) => (
-                      <Vulnerabilities {...props} groupBy="title" />
-                    )}
-                  />
-                  <RouteGuard
-                    path="/inventory/vulnerability/:vulnerabilityId"
-                    component={Vulnerability}
-                  />
+                <RouteGuard exact path="/inventory" component={SearchPage} />
+                <RouteGuard
+                  path="/inventory/domain/:domainId"
+                  component={Domain}
+                />
+                <RouteGuard path="/inventory/domains" component={Domains} />
+                <RouteGuard
+                  path="/inventory/vulnerabilities"
+                  exact
+                  component={Vulnerabilities}
+                />
+                <RouteGuard
+                  path="/inventory/vulnerabilities/grouped"
+                  component={(props) => (
+                    <Vulnerabilities {...props} groupBy="title" />
+                  )}
+                />
+                <RouteGuard
+                  path="/inventory/vulnerability/:vulnerabilityId"
+                  component={Vulnerability}
+                />
 
-                  <RouteGuard path="/feeds" component={Feeds} />
-                  <RouteGuard path="/reports" component={Reports} />
-                  <RouteGuard path="/scans" component={Scans} exact />
-                  <RouteGuard path="/scans/history" component={Scans} exact />
-                  <RouteGuard path="/scans/:scanId" component={Scan} />
-                  <RouteGuard
-                    path="/organizations/:organizationId"
-                    component={Organization}
-                  />
-                  <RouteGuard path="/organizations" component={Organizations} />
-                  <RouteGuard path="/users" component={Users} />
-                  <RouteGuard path="/settings" component={Settings} />
-                </Switch>
-              </Layout>
-            </SearchProvider>
-          </Authenticator.Provider>
-        </AuthContextProvider>
-      </CFThemeProvider>
-    </Router>
-  </MatomoProvider>
+                <RouteGuard path="/feeds" component={Feeds} />
+                <RouteGuard path="/reports" component={Reports} />
+                <RouteGuard path="/scans" component={Scans} exact />
+                <RouteGuard path="/scans/history" component={Scans} exact />
+                <RouteGuard path="/scans/:scanId" component={Scan} />
+                <RouteGuard
+                  path="/organizations/:organizationId"
+                  component={Organization}
+                />
+                <RouteGuard path="/organizations" component={Organizations} />
+                <RouteGuard path="/users" component={Users} />
+                <RouteGuard path="/settings" component={Settings} />
+              </Switch>
+            </Layout>
+          </SearchProvider>
+        </Authenticator.Provider>
+      </AuthContextProvider>
+    </CFThemeProvider>
+  </Router>
+  // </MatomoProvider>
 );
 
 export default App;
