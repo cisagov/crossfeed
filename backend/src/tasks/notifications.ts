37d6a50bacb1851596733d73f95d6d282dc3c564
rc-raizen <rexcazaubon340@gmail.com>
2023-03-16 13:35:04 -0400

    emailing test updates

--- backend/src/tasks/notifications.ts
+++ backend/src/tasks/notifications.ts
@@ -1,10 +1,6 @@
 import { Handler } from 'aws-lambda';
 import { sendEmail } from '../api/helpers';
-import {
-  connectToDatabase,
-  Organization,
-  Role,
-} from '../models';
+import { connectToDatabase, Organization, Role } from '../models';
 import S3Client from './s3-client';
 
 const sendReportEmail = async (email: string, organization?: Organization) => {
@@ -24,60 +20,54 @@ For more information on using Crossfeed, view the Crossfeed user guide at https:
 If you encounter any difficulties, please feel free to reply to this email (or send an email to ${
       process.env.CROSSFEED_SUPPORT_EMAIL_REPLYTO
     }).`
-  );};
+  );
+};
 
 export const handler: Handler = async (event) => {
   await connectToDatabase();
   console.log('Running notifications check...');
-  
+
   // Get all organizations
   const organizations = await Organization.find();
 
   for (const org in organizations) {
     const client = new S3Client();
 
-    // pull reports list 
+    // pull reports list
     const reportsList = await client.listReports(organizations[org].id);
-    const list = JSON.stringify(reportsList)
     // find org admin roles for each organization
-    const roles = await Role.find(
-      {
-        relations: ['user', 'organization'],
-        where: {
-          role: 'admin',
-          organization: organizations[org],
-        },
+    const roles = await Role.find({
+      relations: ['user', 'organization'],
+      where: {
+        role: 'admin',
+        organization: organizations[org]
       }
-    );
+    });
 
-    // loop through reports 
+    // loop through reports
     for (const report in reportsList) {
 
-      if (reportsList[report].LastModified >= organizations[org].notifiedAt){
-
-      for (const role in roles) {
-    
-      // email reporting -- TODO need actual org name thats in database here
-      if (roles[role].organization.name.startsWith('Web')) {
-            // TODO - pulling orgadmin email to send emails
+      if (reportsList[report].LastModified >= organizations[org].notifiedAt) {
+        for (const role in roles) {
+          // email reporting -- TODO need actual org name thats in database here
+          if (roles[role].organization.name.startsWith('Web')) {
             await sendReportEmail(roles[role].user.email, organizations[org]);
-            organizations[org].notifiedAt = new Date()
+            organizations[org].notifiedAt = new Date();
 
-        // P&E
-      } else if (roles[role].organization.name.startsWith('Posture')) {
-           await sendReportEmail(roles[role].user.email, organizations[org]);  
-           organizations[org].notifiedAt = new Date()
+            // P&E
+          } else if (roles[role].organization.name.startsWith('Posture')) {
+            await sendReportEmail(roles[role].user.email, organizations[org]);
+            organizations[org].notifiedAt = new Date();
 
-        // VS
-      } else if (roles[role].organization.name.startsWith('Vulnerability')) {
-           await sendReportEmail(roles[role].user.email, organizations[org]);
-           organizations[org].notifiedAt = new Date()
-
-    }
+            // VS
+          } else if (
+            roles[role].organization.name.startsWith('Vulnerability')
+          ) {
+            await sendReportEmail(roles[role].user.email, organizations[org]);
+            organizations[org].notifiedAt = new Date();
+          }
+        }
+      }
     }
   }
-  } 
-  
-  }
-
 };
--- backend/src/tasks/test/notifications.test.ts
+++ backend/src/tasks/test/notifications.test.ts
@@ -1,8 +1,14 @@
-import { handler as notifications} from '../notifications';
+import { handler as notifications } from '../notifications';
 import app from '../../../src/api/app';
 import * as request from 'supertest';
 import { createUserToken } from '../../../test/util';
-import { connectToDatabase, Organization, Role, User, UserType } from '../../models';
+import {
+  connectToDatabase,
+  Organization,
+  Role,
+  User,
+  UserType
+} from '../../models';
 
 jest.mock('../s3-client');
 const listReports = require('../s3-client').listReports as jest.Mock;
@@ -11,112 +17,115 @@ const nodemailer = require('nodemailer');
 
 const sendMailMock = jest.fn();
 jest.mock('nodemailer');
-nodemailer.createTransport.mockReturnValue({ 'sendMail': sendMailMock});
+nodemailer.createTransport.mockReturnValue({ sendMail: sendMailMock });
 
 beforeEach(() => {
   sendMailMock.mockClear();
   nodemailer.createTransport.mockClear();
-})
+});
 
-describe ('notifications', () => {
-   let organization;
-   let organization2;
-   let organization3;
-    beforeAll(async () => {
-        await connectToDatabase();
-        organization = await Organization.create({
-          name: 'Web Application Scanning-' + Math.random(),
-          rootDomains: ['WAS-' + Math.random()],
-          ipBlocks: [],
-          isPassive: false
-        }).save();
-       
-        organization2 = await Organization.create({
-          name: 'Posture & Exposure-' + Math.random(),
-          rootDomains: ['P&E-' + Math.random()],
-          ipBlocks: [],
-          isPassive: false
-        }).save();
-        
-        organization3 = await Organization.create({
-          name: 'Vulnerability Scanning-' + Math.random(),
-          rootDomains: ['VS-' + Math.random()],
-          ipBlocks: [],
-          isPassive: false
-        }).save();
-    });
-    test('list all organizations', async () => {
-       
-        const response = await request(app)
-            .get(`/organizations`)
-            .set(
-              'Authorization',
-              createUserToken({
-                userType: UserType.GLOBAL_ADMIN
-              })
-            )
-            .expect(200);
-          expect(response.body.length).toBe(3); 
-    });
-    
-     test('Get all org admins only', async () => {
-        
-        const user = await User.create ({
-            firstName: 'Deku',
-            lastName: '',
-            email: Math.random() + '@crossfeed.cisa.gov',
-            
-        }).save();
-        const user2 = await User.create ({
-          firstName: 'Kacchan',
-          lastName: '',
-          email: Math.random() + '@crossfeed.cisa.gov',
-      }).save();
-      const user3 = await User.create ({
-        firstName: 'Todoroki',
-        lastName: '',
-        email: Math.random() + '@crossfeed.cisa.gov',
-    }).save();
-        await Role.create({
-            role: 'admin',
-            approved: false,
-            organization: organization,
-            user: user
-        }).save();
-        await Role.create({
-          role: 'admin',
-          approved: false,
-          organization: organization2,
-          user: user2
-      }).save();
-      await Role.create({
-        role: 'admin',
-        approved: false,
-        organization: organization3,
-        user: user3,
+describe('notifications', () => {
+  let organization;
+  let organization2;
+  let organization3;
+  beforeAll(async () => {
+    await connectToDatabase();
+    organization = await Organization.create({
+      name: 'Web Application Scanning-' + Math.random(),
+      rootDomains: ['WAS-' + Math.random()],
+      ipBlocks: [],
+      isPassive: false
     }).save();
 
-        
-        const response = await request(app)
-          .get(`/organizations`)  
-          .set(
-            'Authorization',
-            createUserToken({
-              roles: [{org: organization.id, role: 'admin'}],
-              
-            })
-          )
-          .send({currentOrganization: {id: organization.id}})
-          .expect(200)
-        expect(response)
+    organization2 = await Organization.create({
+      name: 'Posture & Exposure-' + Math.random(),
+      rootDomains: ['P&E-' + Math.random()],
+      ipBlocks: [],
+      isPassive: false
+    }).save();
 
-    }); 
-    
-    test('getting reports list for all organizations', async () => {
-      const user = await User.create ({
-        firstName: 'Dabi',
-        lastName: '',
-        email: Math.random() + '@crossfeed.cisa.gov', 
+    organization3 = await Organization.create({
+      name: 'Vulnerability Scanning-' + Math.random(),
+      rootDomains: ['VS-' + Math.random()],
+      ipBlocks: [],
+      isPassive: false
+    }).save();
+  });
+  test('list all organizations', async () => {
+    const response = await request(app)
+      .get(`/organizations`)
+      .set(
+        'Authorization',
+        createUserToken({
+          userType: UserType.GLOBAL_ADMIN
+        })
+      )
+      .expect(200);
+    expect(response.body.length).toBe(3);
+  });
+
+  test('Get org admins ', async () => {
+    const firstName = 'first names';
+    const lastName = 'last names';
+    const email = Math.random() + '@crossfeed.cisa.gov';
+    const user = await User.create({
+      firstName: 'Deku',
+      lastName: '',
+      email: Math.random() + '@crossfeed.cisa.gov'
+    }).save();
+    const user2 = await User.create({
+      firstName: 'Kacchan',
+      lastName: '',
+      email: Math.random() + '@crossfeed.cisa.gov'
+    }).save();
+    const user3 = await User.create({
+      firstName: 'Todoroki',
+      lastName: '',
+      email: Math.random() + '@crossfeed.cisa.gov'
+    }).save();
+    await Role.create({
+      role: 'admin',
+      approved: false,
+      organization: organization,
+      user: user
+    }).save();
+    await Role.create({
+      role: 'admin',
+      approved: false,
+      organization: organization2,
+      user: user2
+    }).save();
+    await Role.create({
+      role: 'user',
+      approved: false,
+      organization: organization3,
+      user: user3
+    }).save();
+
+    const response = await request(app)
+      .post(`/users`)
+      .set(
+        'Authorization',
+        createUserToken({
+          roles: [{ org: organization.id, role: 'admin' }]
+        })
+      )
+      .send({
+        firstName,
+        lastName,
+        email,
+        organization: organization.id,
+        organizationAdmin: true})
+      .expect(200);
+      console.log(response.body)
+      expect(response.body.roles[0].role).toEqual('admin'); // this may be key
+    });
+
+  test('getting reports list for all organizations', async () => {
+    const user = await User.create({
+      firstName: 'Dabi',
+      lastName: '',
+      email: Math.random() + '@crossfeed.cisa.gov'
     }).save();
     await Role.create({
       role: 'user',
@@ -124,95 +133,79 @@ describe ('notifications', () => {
       organization,
       user
     }).save();
-        await notifications(
-          {},
-          {} as any,
-          () => void 0
-          )
 
-        const response = await request(app)
-          .get('/reports/list')
-          .set(
-            'Authorization',
-            createUserToken({
-              roles: [{ org: organization.id, role: 'user' }]
-            })
-          )
-          .send({ currentOrganization: { id: organization.id } })
-          .expect(200);
-        //expect(response.text).toEqual('{"Contents":"report content"}');
-        expect(listReports).toBeCalled;
-    
-    });
-    test('Emails to regular user should not send', async () => {
-      const firstName = 'first names';
-      const lastName = 'last names';
-      const email = Math.random() + '@crossfeed.cisa.gov'; 
-      const user = await User.create ({
-          firstName,
-          lastName,
-          email,
-      }).save();
-
-      await notifications(
-        {
-          notifications
-        },
-        {} as any,
-        () => void 0
-      );
-      const response = await request(app)
-        .post('/users')  
-        .set(
-          'Authorization',
-          createUserToken({
-            roles: [{org: organization.id, role: 'user'}],
-            
-          })
-        )
-        .send({firstName,
-               lastName,
-               email,
-               organization: organization.id,
-               organizationAdmin: false
-              })
-        .expect(403)
-
-  });
-  test('Emails to org admin user should send', async () => {
-
-    const firstName = 'first names';
-    const lastName = 'last names';
-    const email = Math.random() + '@crossfeed.cisa.gov'; 
-    const user = await User.create ({
-        firstName,
-        lastName,
-        email,
-    }).save();
-   
     const response = await request(app)
-      .post('/users')  
+      .get('/reports/list')
       .set(
         'Authorization',
         createUserToken({
-          roles: [{org: organization.id, role: 'admin'}],
+          roles: [{ org: organization.id, role: 'user' }]
         })
       )
-      .send({firstName,
-             lastName,
-             email,
-             organization: organization.id,
-             organizationAdmin: true
-            })
-      .expect(200)
-    console.log(response.body)
+      .send({ currentOrganization: { id: organization.id } })
+      .expect(200);
+    expect(response.text).toEqual('{"Contents":"report content"}');
+    expect(listReports).toBeCalled;
+  });
+  test('Emails to regular user should not send', async () => {
+    const firstName = 'first names';
+    const lastName = 'last names';
+    const email = Math.random() + '@crossfeed.cisa.gov';
+    const user = await User.create({
+      firstName,
+      lastName,
+      email
+    }).save();
+
+    const response = await request(app)
+      .post('/users')
+      .set(
+        'Authorization',
+        createUserToken({
+          roles: [{ org: organization.id, role: 'user' }]
+        })
+      )
+      .send({
+        firstName,
+        lastName,
+        email,
+        organization: organization.id,
+        organizationAdmin: false
+      })
+      .expect(403);
+  });
+  test('Emails to org admin user should send', async () => {
+    const firstName = 'first names';
+    const lastName = 'last names';
+    const email = Math.random() + '@crossfeed.cisa.gov';
+    const user = await User.create({
+      firstName,
+      lastName,
+      email
+    }).save();
+
+    const response = await request(app)
+      .post('/users')
+      .set(
+        'Authorization',
+        createUserToken({
+          roles: [{ org: organization.id, role: 'admin' }]
+        })
+      )
+      .send({
+        firstName,
+        lastName,
+        email,
+        organization: organization.id,
+        organizationAdmin: true
+      })
+      .expect(200);
+    //console.log(response.body)
     expect(response.body.email).toEqual(email);
     expect(response.body.firstName).toEqual(firstName);
     expect(response.body.lastName).toEqual(lastName);
     expect(response.body.roles[0].approved).toEqual(true);
     expect(response.body.roles[0].role).toEqual('admin');
     expect(response.body.roles[0].organization.id).toEqual(organization.id);
-
-
-});    
+  });
 });
