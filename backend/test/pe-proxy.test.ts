098edd628dad014fc15f89ee4ec6354fcad84f84
aloftus23 <andylofti@gmail.com>
2023-03-24 08:59:17 -0400

    Use nock to mock http request

--- backend/test/pe-proxy.test.ts
+++ backend/test/pe-proxy.test.ts
@@ -3,19 +3,30 @@ import app from '../src/api/app';
 import { createUserToken } from './util';
 import { connectToDatabase, UserType } from '../src/models';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+// import nock from 'nock';
 
-jest.mock('http-proxy-middleware', () => ({
-  createProxyMiddleware: jest.fn()
-}));
+const nock = require('nock');
+
+// const createProxyMiddleware = jest.fn();
 
 describe('pe-proxy', () => {
   beforeAll(async () => {
     await connectToDatabase();
-    (createProxyMiddleware as jest.Mock).mockImplementation(() => 200);
+    nock('http://fakePeApi.com')
+      .get('/api')
+      .reply(200, { message: 'Hello from fake pe server' });
   });
   it('standard user is not authorized to access P&E proxy', async () => {
+    app.use(
+      '/pe',
+      createProxyMiddleware({
+        target: 'http://fakePeApi.com',
+        changeOrigin: true
+      })
+    );
+
     const response = await request(app)
-      .get('/pe')
+      .get('/pe/api')
       .set(
         'Authorization',
         createUserToken({
@@ -24,10 +35,19 @@ describe('pe-proxy', () => {
       )
       .expect(401);
     expect(response.text).toEqual('Unauthorized');
+    // expect(createProxyMiddleware).toBeCalledTimes(0);
   });
   it('global admin is authorized to access P&E proxy', async () => {
+    app.use(
+      '/pe',
+      createProxyMiddleware({
+        target: 'http://fakePeApi.com',
+        changeOrigin: true
+      })
+    );
+
     const response = await request(app)
-      .get('/pe')
+      .get('/pe/api')
       .set(
         'Authorization',
         createUserToken({
@@ -37,8 +57,16 @@ describe('pe-proxy', () => {
       .expect(200);
   });
   it('global view user is authorized to access P&E proxy', async () => {
+    app.use(
+      '/pe',
+      createProxyMiddleware({
+        target: 'http://fakePeApi.com',
+        changeOrigin: true
+      })
+    );
+
     const response = await request(app)
-      .get('/pe')
+      .get('/pe/api')
       .set(
         'Authorization',
         createUserToken({
