f18ec09536415df586a0b5f18a63fe03a6e5f971
aloftus23 <andylofti@gmail.com>
2023-03-24 10:07:28 -0400

    add nock to intercept any http requests

--- backend/test/pe-proxy.test.ts
+++ backend/test/pe-proxy.test.ts
@@ -3,11 +3,15 @@ import app from '../src/api/app';
 import { createUserToken } from './util';
 import { connectToDatabase, UserType } from '../src/models';
 
+const nock = require('nock');
+
 describe('pe-proxy', () => {
   beforeAll(async () => {
     await connectToDatabase();
     process.env.PE_API_URL = 'http://localhost:8080';
+    nock('http://localhost:8080').get('/pe').reply(200, 'OK');
   });
+
   it('standard user is not authorized to access P&E proxy', async () => {
     const response = await request(app)
       .get('/pe')
@@ -20,6 +24,7 @@ describe('pe-proxy', () => {
       .expect(401);
     expect(response.text).toEqual('Unauthorized');
   });
+
   it('gloabl admin is authorized to access P&E proxy', async () => {
     const response = await request(app)
       .get('/pe')
@@ -31,6 +36,7 @@ describe('pe-proxy', () => {
       );
     expect(response.status).toBe(200);
   });
+
   it('gloabl view user is authorized to access P&E proxy', async () => {
     const response = await request(app)
       .get('/pe')
