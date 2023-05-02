0aa53e34af565956b70fd029f06e7957d43cc7a6
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-05-02 13:52:31 -0500

    Results for each domain are appended to a single csv file per organization; but headers are duplicated

--- backend/src/tasks/trustymail.ts
+++ backend/src/tasks/trustymail.ts
@@ -2,11 +2,16 @@ import getAllDomains from './helpers/getAllDomains';
 import { spawnSync } from 'child_process';
 import { CommandOptions } from './ecs-client';
 import * as path from 'path';
+import * as fs from 'fs';
 
-const OUT_PATH = path.join(__dirname, 'out-' + Math.random() + '.txt');
-
+// TODO: Scan results are concatenated to a single file per organization.
+// The header is duplicated for each domain so we need to either remove duplicates or skip the header when reading the results.csv for each domain.
+const csvHeader = `Domain,Base Domain,Live,MX Record,MX Record DNSSEC,Mail Servers,Mail Server Ports Tested,Domain Supports SMTP Results,Domain Supports SMTP,Domain Supports STARTTLS Results,Domain Supports STARTTLS,SPF Record,SPF Record DNSSEC,Valid SPF,SPF Results,DMARC Record,DMARC Record DNSSEC,Valid DMARC,DMARC Results,DMARC Record on Base Domain,DMARC Record on Base Domain DNSSEC,Valid DMARC Record on Base Domain,DMARC Results on Base Domain,DMARC Policy,DMARC Subdomain Policy,DMARC Policy Percentage,DMARC Aggregate Report URIs,DMARC Forensic Report URIs,DMARC Has Aggregate Report URI,DMARC Has Forensic Report URI,DMARC Reporting Address Acceptance Error,Syntax Errors,Debug Info
+`;
 export const handler = async (commandOptions: CommandOptions) => {
   const { organizationId, organizationName, scanId } = commandOptions;
+  const OUT_PATH = path.join(__dirname, 'out-' + Math.random() + '.csv');
+  fs.writeFileSync(OUT_PATH, csvHeader);
 
   console.log('Running trustymail on organization', organizationName);
 
@@ -15,15 +20,16 @@ export const handler = async (commandOptions: CommandOptions) => {
 
   for (const domain of domains) {
     try {
-      const args = [domain.name, '-o', OUT_PATH];
+      const args = [domain.name];
       console.log('Running trustymail with args', args);
       spawnSync('trustymail', args, { stdio: 'pipe' });
-      console.log(
-        `Findomain scanned ${domains.length} domains for ${organizationName}`
-      );
+      fs.appendFileSync(OUT_PATH, String(fs.readFileSync('results.csv')));
     } catch (e) {
       console.error(e);
       continue;
     }
   }
+  console.log(
+    `Trustymail scanned ${domains.length} domains for ${organizationName}`
+  );
 };
