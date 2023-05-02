bae6340cfcf7d756bc7024c72b5eacd180dce501
colin-tim <82547571+colin-tim@users.noreply.github.com>
2023-05-02 09:08:02 -0700

    Moving back to node 18 (#1854)
    * Moving back to node 18. Fix some issues related to the upgrade. The header property in jsonwebtoken is not required as it will default to the existing value. Fixed an issue with index not recognized in services entity. Removed some not required py packages.
    
    * Adding missing package to fix lint test
    
    * Fixing lint errors.
    
    * Remove the no-cache option.
    
    * Remove telerik related wappalyzer tests. Saving the tests to a backup file to be added later.
    
    * Revert to original pe-reports sync cmd.

--- backend/src/tasks/test/amass.test.ts
+++ backend/src/tasks/test/amass.test.ts
@@ -3,7 +3,8 @@ jest.mock('../helpers/getRootDomains');
 jest.mock('../helpers/saveDomainsToDb');
 
 jest.mock('fs', () => ({
-  readFileSync: () => `{"name":"filedrop.cisa.gov","domain":"cisa.gov","addresses":[{"ip":"2a02:26f0:7a00:195::447a","cidr":"2a02:26f0:7a00::/48","asn":6762,"desc":"SEABONE-NET TELECOM ITALIA SPARKLE S.p.A."},{"ip":"2a02:26f0:7a00:188::447a","cidr":"2a02:26f0:7a00::/48","asn":6762,"desc":"SEABONE-NET TELECOM ITALIA SPARKLE S.p.A."}],"tag":"dns","source":"DNS"}
+  readFileSync:
+    () => `{"name":"filedrop.cisa.gov","domain":"cisa.gov","addresses":[{"ip":"2a02:26f0:7a00:195::447a","cidr":"2a02:26f0:7a00::/48","asn":6762,"desc":"SEABONE-NET TELECOM ITALIA SPARKLE S.p.A."},{"ip":"2a02:26f0:7a00:188::447a","cidr":"2a02:26f0:7a00::/48","asn":6762,"desc":"SEABONE-NET TELECOM ITALIA SPARKLE S.p.A."}],"tag":"dns","source":"DNS"}
 {"name":"www.filedrop.cisa.gov","domain":"cisa.gov","addresses":[{"ip":"2a02:26f0:7a00:188::447a","cidr":"2a02:26f0:7a00::/48","asn":6762,"desc":"SEABONE-NET TELECOM ITALIA SPARKLE S.p.A."},{"ip":"2a02:26f0:7a00:195::447a","cidr":"2a02:26f0:7a00::/48","asn":6762,"desc":"SEABONE-NET TELECOM ITALIA SPARKLE S.p.A."}],"tag":"cert","source":"Crtsh"}`,
   unlinkSync: () => null
 }));
