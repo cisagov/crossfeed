b34f7da89794d224d966d9940eb54049d34a0c70
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-05-04 15:50:04 -0500

    Add function call to save scan results to database.

--- backend/src/tasks/trustymail.ts
+++ backend/src/tasks/trustymail.ts
@@ -1,9 +1,11 @@
 import getAllDomains from './helpers/getAllDomains';
 import { spawn, spawnSync } from 'child_process';
 import { CommandOptions } from './ecs-client';
+import saveTrustymailResultsToDb from './helpers/saveTrustymailResultsToDb';
 
 // TODO: Push results to domain table
 // TODO: Implement p-queue
+// TODO: Add scan time to domain table
 // TODO: Test using spawn in place of spawnSync once p-queue is implemented
 export const handler = async (commandOptions: CommandOptions) => {
   const { organizationId, organizationName } = commandOptions;
@@ -20,6 +22,7 @@ export const handler = async (commandOptions: CommandOptions) => {
       const args = [domain.name, '--json', '--debug', `--output=${domain.id}`];
       console.log('Running trustymail with args:', args);
       spawnSync('trustymail', args, { stdio: 'pipe' });
+      saveTrustymailResultsToDb(domain.id, `${domain.id}.json`);
     } catch (e) {
       console.error(e);
       continue;
