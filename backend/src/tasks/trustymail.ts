a0794edc73adac734228034d92072e9befb303ae
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-05-04 13:30:13 -0500

    Add draft field for trustymail results.

--- backend/src/tasks/trustymail.ts
+++ backend/src/tasks/trustymail.ts
@@ -1,35 +1,30 @@
 import getAllDomains from './helpers/getAllDomains';
-import { spawnSync } from 'child_process';
+import { spawn, spawnSync } from 'child_process';
 import { CommandOptions } from './ecs-client';
-import * as path from 'path';
-import * as fs from 'fs';
 
-// TODO: Scan results are concatenated to a single file per organization.
-// The header is duplicated for each domain so we need to either remove duplicates or skip the header when reading the results.csv for each domain.
-const csvHeader = `Domain,Base Domain,Live,MX Record,MX Record DNSSEC,Mail Servers,Mail Server Ports Tested,Domain Supports SMTP Results,Domain Supports SMTP,Domain Supports STARTTLS Results,Domain Supports STARTTLS,SPF Record,SPF Record DNSSEC,Valid SPF,SPF Results,DMARC Record,DMARC Record DNSSEC,Valid DMARC,DMARC Results,DMARC Record on Base Domain,DMARC Record on Base Domain DNSSEC,Valid DMARC Record on Base Domain,DMARC Results on Base Domain,DMARC Policy,DMARC Subdomain Policy,DMARC Policy Percentage,DMARC Aggregate Report URIs,DMARC Forensic Report URIs,DMARC Has Aggregate Report URI,DMARC Has Forensic Report URI,DMARC Reporting Address Acceptance Error,Syntax Errors,Debug Info
-`;
+// TODO: Test using spawn in place of spawnSync
+const orgMap = new Map<string, string>();
 export const handler = async (commandOptions: CommandOptions) => {
   const { organizationId, organizationName, scanId } = commandOptions;
-  const OUT_PATH = path.join(__dirname, 'out-' + Math.random() + '.csv');
-  fs.writeFileSync(OUT_PATH, csvHeader);
-
+  orgMap[organizationId!] = new Map<string, string>();
   console.log('Running trustymail on organization', organizationName);
 
   const domains = await getAllDomains([organizationId!]);
-  console.log(`${organizationName} domains`, domains);
+  const domainNames = domains.map((domain) => domain.name);
+  const domainCount = domainNames.length;
+  console.log(`${organizationName} domains:`, domainNames);
 
   for (const domain of domains) {
     try {
-      const args = [domain.name];
+      const args = [domain.name, '--json', '--debug', `--output=${domain.id}`];
       console.log('Running trustymail with args', args);
       spawnSync('trustymail', args, { stdio: 'pipe' });
-      fs.appendFileSync(OUT_PATH, String(fs.readFileSync('results.csv')));
     } catch (e) {
       console.error(e);
       continue;
     }
   }
   console.log(
-    `Trustymail scanned ${domains.length} domains for ${organizationName}`
+    `Trustymail scanned ${domainCount} domains for ${organizationName}`
   );
 };
