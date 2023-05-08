77913733d0f6480109de5d2163491720a80cdf89
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-05-08 16:38:17 -0500

    Remove fs import.

--- backend/src/tasks/helpers/saveTrustymailResultsToDb.ts
+++ backend/src/tasks/helpers/saveTrustymailResultsToDb.ts
@@ -1,6 +1,5 @@
 import { domain } from 'process';
 import { connectToDatabase, Domain } from '../../models';
-import * as fs from 'fs';
 
 export default async (domainId: string, jsonData: Buffer): Promise<void> => {
   await connectToDatabase();
