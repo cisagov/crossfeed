1c7e87b3650aadb6a9b62b47e5b47bb00f22ae4a
aloftus23 <79927030+aloftus23@users.noreply.github.com>
2023-05-16 12:04:39 -0400

    Validate organization ID in reports/export endpoint (#1899)
    * Pass org_id to the the getSignedURL function
    
    * Update variable names and remove logging
    
    * Update s3-client test to accept 2 arguments

--- backend/src/api/reports.ts
+++ backend/src/api/reports.ts
@@ -7,16 +7,16 @@ import { getOrgMemberships } from './auth';
  *
  * /reports/export:
  *  post:
- *    description: Export CyHy report by specifying the S3 key returned in /reports/list and the organization id. User must be a member of the organization.
+ *    description: Export CyHy report by specifying the report name returned in /reports/list and the organization id. User must be a member of the organization.
  *    tags:
  *    - Reports
  */
 export const export_report = wrapHandler(async (event) => {
-  const Key = JSON.parse(event.body!).Key;
+  const reportName = JSON.parse(event.body!).reportName;
   const orgId = JSON.parse(event.body!).currentOrganization.id;
   if (getOrgMemberships(event).includes(orgId)) {
     const client = new S3Client();
-    const url = await client.exportReport(Key);
+    const url = await client.exportReport(reportName, orgId);
     if (url == 'File does not exist') {
       return {
         statusCode: 404,
