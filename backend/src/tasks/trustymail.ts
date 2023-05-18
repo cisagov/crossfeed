aed57b11a13613d13b18f76a5557592675e2f5c8
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-05-18 12:47:00 -0500

    Remove event listener and associated checks for public_suffix_list.dat.

--- backend/src/tasks/trustymail.ts
+++ backend/src/tasks/trustymail.ts
@@ -4,21 +4,14 @@ import { CommandOptions } from './ecs-client';
 import saveTrustymailResultsToDb from './helpers/saveTrustymailResultsToDb';
 import * as chokidar from 'chokidar';
 
-// TODO: Add scan time to domain table
 // TODO: Retry failed domains
-// TODO: PSL is downloaded once per organization per scan; would like to download only once per day
+// TODO: PSL is downloaded once per container (number of orgs / 2, rounded up); would like to download only once per day
 export const handler = async (commandOptions: CommandOptions) => {
-  let pslDownloaded = false;
   const { organizationId, organizationName } = commandOptions;
   console.log('Running Trustymail on organization', organizationName);
   const domains = await getAllDomains([organizationId!]);
   const queue = new Set(domains.map((domain) => `${domain.id}.json`));
   const failedDomains = new Set<string>();
-  // Event listener detects when public_suffix_list.dat is downloaded
-  chokidar.watch('public_suffix_list.dat').on('add', () => {
-    console.log('Public suffix list downloaded');
-    pslDownloaded = true;
-  });
   console.log(
     `${organizationName} domains to be scanned:`,
     domains.map((domain) => domain.name)
@@ -41,7 +34,7 @@ export const handler = async (commandOptions: CommandOptions) => {
         domain.name,
         '--json',
         `--output=${domain.id}`,
-        '--psl-file=public_suffix_list.dat'
+        '--psl-filename=public_suffix_list.dat'
       ];
       console.log('Running Trustymail with args:', args);
       const child = spawn('trustymail', args, { stdio: 'pipe' });
@@ -52,30 +45,21 @@ export const handler = async (commandOptions: CommandOptions) => {
         path,
         failedDomains
       );
-      // Wait for PSL download and continue to next domain if it takes more than 10 seconds
-      let pslDownloadTimeout = 0;
-      while (!pslDownloaded && pslDownloadTimeout < 10) {
-        console.log('Waiting for public_suffix_list.dat to be generated...');
-        await delay(1000 * 1);
-        pslDownloadTimeout++;
-      }
     } catch (e) {
       console.error(e);
-      continue;
     }
   }
   // Keep container alive while syncing results to db; timeout after 10 minutes (60 * 10 seconds)
   let dbSyncTimeout = 0;
   while (queue.size > 0 && dbSyncTimeout < 60) {
-    console.log('Waiting for Trustymail results to sync to db...');
-    console.log('queue', queue);
-    await delay(1000 * 10);
+    console.log('Syncing...');
+    await delay(1000 * 2);
     dbSyncTimeout++;
   }
   console.log(
     `Trustymail successfully scanned ${
       domains.length - failedDomains.size
-    } domains out of ${domains.length} domains \n failed domains:`,
+    } out of ${domains.length} domains \n failed domains:`,
     failedDomains
   );
 };
