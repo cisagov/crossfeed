0055c876c8dc449563461cbc5c7283ad0975f820
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-05-18 14:57:03 -0500

    Update jsonObject to match domain model (remove outer square brackets); refactor timestamp to updatedAt.

--- backend/src/tasks/helpers/saveTrustymailResultsToDb.ts
+++ backend/src/tasks/helpers/saveTrustymailResultsToDb.ts
@@ -5,8 +5,8 @@ import * as fs from 'fs';
 // TODO: Find a way to only send one query to the db
 export default async (path: string): Promise<void> => {
   const jsonData = await fs.promises.readFile(path, 'utf8');
-  const jsonObject = JSON.parse(jsonData);
-  jsonObject.push({ Timestamp: new Date() });
+  const jsonObject = JSON.parse(jsonData)[0];
+  jsonObject.updatedAt = new Date();
   const domainId = path.split('/')[path.split('/').length - 1].split('.')[0];
   await connectToDatabase();
   await Domain.createQueryBuilder()
