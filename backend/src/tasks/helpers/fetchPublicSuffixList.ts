b0330a0a69a8c37f9712e324ab6755d386c99d56
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-05-19 15:35:32 -0500

    Refactor to pass codeQL scans.

--- backend/src/tasks/helpers/fetchPublicSuffixList.ts
+++ backend/src/tasks/helpers/fetchPublicSuffixList.ts
@@ -5,16 +5,11 @@ export default async (path = 'public_suffix_list.dat'): Promise<void> => {
   fs.open(path, 'wx', async (err, fd) => {
     if (err) {
       if (err.code === 'EEXIST') {
-        const pslAge =
-          (Date.now() - fs.statSync(path).mtimeMs) / (1000 * 60 * 60);
-        if (pslAge > 24) {
-          fs.writeFileSync(path, await fetchPSL());
-        }
-        return;
+        return await handleExistingPsl(path);
       } else throw err;
     }
     try {
-      fs.writeFileSync(fd, await fetchPSL());
+      fs.writeFileSync(fd, await fetchPsl());
     } catch (e) {
       console.error(e);
     } finally {
@@ -22,8 +17,25 @@ export default async (path = 'public_suffix_list.dat'): Promise<void> => {
     }
   });
 };
+async function handleExistingPsl(path) {
+  fs.open(path, 'r+', async (err, fd) => {
+    try {
+      const pslAge = (Date.now() - fs.fstatSync(fd).mtimeMs) / (1000 * 60 * 60);
+      if (pslAge > 24) {
+        fs.writeFileSync(fd, await fetchPsl());
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      fs.closeSync(fd);
+    }
+    if (err) {
+      throw err;
+    }
+  });
+}
 
-async function fetchPSL() {
+async function fetchPsl() {
   return await fetch('https://publicsuffix.org/list/public_suffix_list.dat', {
     method: 'GET'
   }).then((res) => res.text());
