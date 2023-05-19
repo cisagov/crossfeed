f30ce58e6fe38fff586679c86a873268163c10b9
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-05-19 13:50:37 -0500

    Refactor to pass codeQL checks.

--- backend/src/tasks/helpers/fetchPublicSuffixList.ts
+++ backend/src/tasks/helpers/fetchPublicSuffixList.ts
@@ -2,18 +2,29 @@ import * as fs from 'fs';
 
 // Checks for PSL at ${path} and fetches a new one if missing or out of date
 export default async (path = 'public_suffix_list.dat'): Promise<void> => {
-  if (!fs.existsSync(path)) {
-    fs.writeFileSync(path, await fetchPSL());
-  } else {
-    const pslAge = (Date.now() - fs.statSync(path).mtimeMs) / (1000 * 60 * 60);
-    if (pslAge > 24) {
-      fs.writeFileSync(path, await fetchPSL());
+  fs.open(path, 'wx', async (err, fd) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        const pslAge =
+          (Date.now() - fs.statSync(path).mtimeMs) / (1000 * 60 * 60);
+        if (pslAge > 24) {
+          fs.writeFileSync(path, await fetchPSL());
+        }
+        return;
+      } else throw err;
     }
-  }
+    try {
+      fs.writeFileSync(fd, await fetchPSL());
+    } catch (e) {
+      console.error(e);
+    } finally {
+      fs.closeSync(fd);
+    }
+  });
 };
 
 async function fetchPSL() {
-  return fetch('https://publicsuffix.org/list/public_suffix_list.dat', {
+  return await fetch('https://publicsuffix.org/list/public_suffix_list.dat', {
     method: 'GET'
   }).then((res) => res.text());
 }
