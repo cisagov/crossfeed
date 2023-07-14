5d1c7fea716f9a3ef65480df12269d2d9ceb221b
ValeriaReveles <valeria.reveles@associates.cisa.dhs.gov>
2023-07-14 08:05:08 -0500

    reinstall package files

--- frontend/src/context/Theme.tsx
+++ frontend/src/context/Theme.tsx
@@ -5,7 +5,7 @@ import {
   Theme,
   StyledEngineProvider,
   adaptV4Theme
-} from '@mui/material/styles';
+} from '@mui/material';
 
 declare module '@mui/styles/defaultTheme' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
