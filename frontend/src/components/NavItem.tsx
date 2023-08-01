0e43e08d7b75ba443f5f05e10ade1ab7859677ae
ValeriaReveles <valeria.reveles@associates.cisa.dhs.gov>
2023-08-01 12:52:02 -0500

    changed 'classes' to 'classesNav' in order to avoid conflict with 'classes' imported from 'classnames'

--- frontend/src/components/NavItem.tsx
+++ frontend/src/components/NavItem.tsx
@@ -1,81 +1,10 @@
 import React, { useState } from 'react';
 import { styled } from '@mui/material/styles';
-import clsx from 'classnames';
+import classes from 'classnames';
 import { NavLink, useRouteMatch, useHistory } from 'react-router-dom';
 import { Menu, MenuItem, Button } from '@mui/material';
 
-const PREFIX = 'NavItem';
 
-const classes = {
-  inner: `${PREFIX}-inner`,
-  menuButton: `${PREFIX}-menuButton`,
-  activeLink: `${PREFIX}-activeLink`,
-  activeMobileLink: `${PREFIX}-activeMobileLink`,
-  link: `${PREFIX}-link`,
-  userLink: `${PREFIX}-userLink`
-};
-
-// TODO jss-to-styled codemod: The Fragment root was replaced by div. Change the tag if needed.
-const Root = styled('div')(({ theme }) => ({
-  [`& .${classes.inner}`]: {
-    maxWidth: 1440,
-    width: '100%',
-    margin: '0 auto'
-  },
-
-  [`& .${classes.menuButton}`]: {
-    marginRight: theme.spacing(2),
-    display: 'block',
-    [theme.breakpoints.up('lg')]: {
-      display: 'none'
-    }
-  },
-
-  [`& .${classes.activeLink}`]: {
-    '&:after': {
-      content: "''",
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      width: '100%',
-      height: 2,
-      backgroundColor: 'white'
-    }
-  },
-
-  [`& .${classes.activeMobileLink}`]: {
-    fontWeight: 700,
-    '&:after': {
-      content: "''",
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      height: '100%',
-      width: 2,
-      backgroundColor: theme.palette.primary.main
-    }
-  },
-
-  [`& .${classes.link}`]: {
-    position: 'relative',
-    color: 'white',
-    textDecoration: 'none',
-    margin: `0 ${theme.spacing()}px`,
-    padding: theme.spacing(),
-    borderBottom: '2px solid transparent',
-    fontWeight: 600
-  },
-
-  [`& .${classes.userLink}`]: {
-    display: 'flex',
-    alignItems: 'center',
-
-    '& svg': {
-      marginRight: theme.spacing()
-    }
-  }
-}));
 
 interface LinkConfig {
   title: string | JSX.Element;
@@ -122,8 +51,8 @@ export const NavItem: React.FC<Props> = (props) => {
       {path ? (
         <NavLink
           to={path}
-          activeClassName={path !== '#' ? classes.activeLink : classes.link}
-          className={classes.link}
+          activeClassName={path !== '#' ? classesNav.activeLink : classesNav.link}
+          className={classesNav.link}
           onClick={onClick ? onClick : onClickButton}
           exact={exact}
           style={{ outline: 'none' }}
@@ -132,8 +61,8 @@ export const NavItem: React.FC<Props> = (props) => {
         </NavLink>
       ) : (
         <Button
-          className={clsx(classes.link, {
-            [classes.activeLink]: !!match
+          className={classes(classesNav.link, {
+            [classesNav.activeLink]: !!match
           })}
           onClick={onClick ? onClick : onClickButton}
           style={{ outline: 'none' }}
@@ -149,11 +78,11 @@ export const NavItem: React.FC<Props> = (props) => {
           onClose={onCloseMenu}
           keepMounted
           anchorOrigin={{
-            vertical: 'bottom',
+            vertical: 'center',
             horizontal: 'center'
           }}
           transformOrigin={{
-            vertical: 'top',
+            vertical: 'center',
             horizontal: 'center'
           }}
         >
@@ -173,3 +102,78 @@ export const NavItem: React.FC<Props> = (props) => {
     </Root>
   );
 };
+
+//Styling
+const PREFIX = 'NavItem';
+
+const classesNav = {
+  inner: `${PREFIX}-inner`,
+  menuButton: `${PREFIX}-menuButton`,
+  activeLink: `${PREFIX}-activeLink`,
+  activeMobileLink: `${PREFIX}-activeMobileLink`,
+  link: `${PREFIX}-link`,
+  userLink: `${PREFIX}-userLink`
+};
+
+// TODO jss-to-styled codemod: The Fragment root was replaced by div. Change the tag if needed.
+const Root = styled('div')(({ theme }) => ({
+  [`& .${classesNav.inner}`]: {
+    maxWidth: 1440,
+    width: '100%',
+    margin: '0 auto',
+    
+  },
+
+  [`& .${classesNav.menuButton}`]: {
+    marginRight: theme.spacing(2),
+    display: 'block',
+    [theme.breakpoints.up('lg')]: {
+      display: 'none'
+    }
+  },
+
+  [`& .${classesNav.activeLink}`]: {
+    '&:after': {
+      content: "''",
+      position: 'absolute',
+      bottom: 0,
+      left: 6,
+      width: '100%',
+      height: 2,
+      backgroundColor: 'white'
+    }
+  },
+
+  [`& .${classesNav.activeMobileLink}`]: {
+    fontWeight: 700,
+    '&:after': {
+      content: "''",
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      height: '100%',
+      width: 2,
+      backgroundColor: theme.palette.primary.main
+    }
+  },
+
+  [`& .${classesNav.link}`]: {
+    position: 'relative',
+    color: 'white',
+    textDecoration: 'none',
+    margin: `0 ${theme.spacing()}px`,
+    padding: theme.spacing(),
+    borderBottom: '2px solid transparent',
+    fontWeight: 600
+  },
+
+  [`& .${classesNav.userLink}`]: {
+    display: 'flex',
+    alignItems: 'center',
+
+    '& svg': {
+      marginRight: theme.spacing()
+    }
+  }
+}));
