5a691fee97c5e7815d20838b088b42b2ec2b8d48
Valeria Reveles <valeria.reveles@associates.cisa.dhs.gov>
2023-08-02 18:55:58 -0500

    updated dependancy in footer and style files

--- frontend/src/components/Footer/style.ts
+++ frontend/src/components/Footer/style.ts
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@mui/material';
 
 export const useFooterStyles = makeStyles((theme) => ({
   footerBox: {
--- frontend/src/pages/Risk/style.ts
+++ frontend/src/pages/Risk/style.ts
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@mui/material';
 
 export const useRiskStyles = makeStyles((theme) => ({
   cardRoot: {
