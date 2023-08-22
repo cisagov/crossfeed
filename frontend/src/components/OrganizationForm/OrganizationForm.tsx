c71146e0cd0d06de02ab8d04018bbb11acc793dd
Valeria Reveles <valeria.reveles@associates.cisa.dhs.gov>
2023-08-22 10:57:34 -0500

    separated style components from 'OrganizationForm' file into own file

--- frontend/src/components/OrganizationForm/OrganizationForm.tsx
+++ frontend/src/components/OrganizationForm/OrganizationForm.tsx
@@ -1,9 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { styled } from '@mui/material/styles';
+import * as orgFormStyles from './orgFormStyle';
 import { Organization, OrganizationTag } from 'types';
 import {
   Autocomplete,
-  Dialog,
   DialogTitle,
   DialogContent,
   TextField,
@@ -19,51 +18,8 @@ import {
 
 import { useAuthContext } from 'context';
 
-const PREFIX = 'OrganizationForm';
-
-const classes = {
-  chip: `${PREFIX}-chip`,
-  headerRow: `${PREFIX}-headerRow`
-};
-
-const StyledDialog = styled(Dialog)(({ theme }) => ({
-  [`& .${classes.chip}`]: {
-    backgroundColor: '#C4C4C4',
-    color: 'white',
-    marginRight: '10px'
-  },
-
-  [`& .${classes.headerRow}`]: {
-    padding: '0.5rem 0',
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    fontSize: '16px',
-    flexWrap: 'wrap',
-    '& label': {
-      flex: '1 0 100%',
-      fontWeight: 'bolder',
-      display: 'flex',
-      alignItems: 'center',
-      padding: '0.5rem 0',
-      '@media screen and (min-width: 640px)': {
-        flex: '0 0 220px',
-        padding: 0
-      }
-    },
-    '& span': {
-      display: 'block',
-      flex: '1 1 auto',
-      marginLeft: 'calc(1rem + 20px)',
-      '@media screen and (min-width: 640px)': {
-        marginLeft: 'calc(1rem + 20px)'
-      },
-      '@media screen and (min-width: 1024px)': {
-        marginLeft: 0
-      }
-    }
-  }
-}));
+const classes = orgFormStyles.classes;
+const StyledDialog = orgFormStyles.StyledDialog;
 
 interface AutocompleteType extends Partial<OrganizationTag> {
   title?: string;
