11eafc25c9abdc37cb063fc6557cba9c20c59992
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-08-23 10:07:03 -0500

    format corrections.

--- infrastructure/cloudtrail.tf
+++ infrastructure/cloudtrail.tf
@@ -1,20 +1,20 @@
 
 resource "aws_cloudwatch_log_group" "all" {
-    name              = var.logging_bucket_name
-    retention_in_days = 3653
-    kms_key_id        = aws_kms_key.key.arn
-    tags = {
-      Project = var.project
-      Stage   = var.stage
+  name              = var.logging_bucket_name
+  retention_in_days = 3653
+  kms_key_id        = aws_kms_key.key.arn
+  tags = {
+    Project = var.project
+    Stage   = var.stage
   }
 }
 
 resource "aws_cloudtrail" "management" {
-  name = "management-events"
-  s3_bucket_name = "management-bucket"
-  aws_cloudwatch_logs_group_arn = "${aws_cloudwatch_log_group.all.arn}:*"
+  name                          = "management-events"
+  s3_bucket_name                = "management-bucket"
+  aws_cloudwatch_logs_group_arn = aws_cloudwatch_log_group.all.arn
   event_selector {
-    read_write_type = "All"
+    read_write_type           = "All"
     include_management_events = true
   }
   enable_log_file_validation = true
@@ -22,9 +22,9 @@ resource "aws_cloudtrail" "management" {
 }
 
 resource "aws_cloudtrail" "data" {
-  name           = "data-events"
-  s3_bucket_name = "data-bucket"
-  aws_cloudwatch_logs_group_arn = "${aws_cloudwatch_log_group.all.arn}:*"
+  name                          = "data-events"
+  s3_bucket_name                = "data-bucket"
+  aws_cloudwatch_logs_group_arn = aws_cloudwatch_log_group.all.arn
   event_selector {
     read_write_type           = "All"
     include_management_events = false
