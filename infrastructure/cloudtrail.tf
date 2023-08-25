3380a25589626e49555aeaab52311602d4bbc7d8
Matthew <106278637+Matthew-Grayson@users.noreply.github.com>
2023-08-25 08:22:28 -0500

    2118 au 05 audit process migrate logs to s3 buckets (#2191)
    * Add CloudTrail log configuration

---
+++ infrastructure/cloudtrail.tf
@@ -0,0 +1,35 @@
+
+resource "aws_cloudwatch_log_group" "all" {
+  name              = var.logging_bucket_name
+  retention_in_days = 3653
+  kms_key_id        = aws_kms_key.key.arn
+  tags = {
+    Project = var.project
+    Stage   = var.stage
+  }
+}
+
+resource "aws_cloudtrail" "all-events" {
+  name                       = "all-events"
+  s3_bucket_name             = var.logging_bucket_name
+  cloud_watch_logs_role_arn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.logging_bucket_name}-cloudtrail-role"
+  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.all.arn
+  tags = {
+    Project = var.project
+    Stage   = var.stage
+  }
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = ["arn:aws:s3"]
+    }
+    data_resource {
+      type   = "AWS::Lambda::Function"
+      values = ["arn:aws:lambda"]
+    }
+  }
+  enable_log_file_validation = true
+  is_multi_region_trail      = true
+}
