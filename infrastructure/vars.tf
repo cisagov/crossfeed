851252374e099a62ef1c6945d65527bf0fd34400
Matthew <106278637+Matthew-Grayson@users.noreply.github.com>
2023-08-28 10:39:48 -0500

    2118 au 05 audit process migrate logs to s3 buckets (#2205)
    * Add ownership controls for cloudtrail bucket. 
    * Add additional vars for bucket name, role, and log group.

--- infrastructure/vars.tf
+++ infrastructure/vars.tf
@@ -268,6 +268,18 @@ variable "cloudtrail_bucket_name" {
   default     = "cisa-crossfeed-staging-cloudtrail"
 }
 
+variable "cloudtrail_role_name" {
+  description = "cloudtrail_role_name"
+  type        = string
+  default     = "crossfeed-staging-cloudtrail-role"
+}
+
+variable "cloudtrail_cloudwatch_log_group_name" {
+  description = "cloudtrail_cloudwatch_log_group_name"
+  type        = string
+  default     = "crossfeed-staging-cloudtrail-logs"
+}
+
 variable "export_bucket_name" {
   description = "export_bucket_name"
   type        = string
