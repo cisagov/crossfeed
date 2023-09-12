b3e133c61c458048d4c5360364d441a611da9833
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-09-12 16:51:48 -0500

    Draft lambda to export cloudwatch logs to s3 bucket; add aws-sdk cloudwatch-logs and ssm to package files.

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -1,11 +1,3 @@
-resource "aws_s3_bucket" "infrastructure" {
-  name = var.infrastructure_bucket_name
-  tags = {
-    Project = var.project
-    Stage   = var.stage
-  }
-}
-
 resource "aws_cloudwatch_log_group" "infrastructure" {
   name              = var.infrastructure_log_group_name
   retention_in_days = 3653
@@ -13,5 +5,40 @@ resource "aws_cloudwatch_log_group" "infrastructure" {
   tags = {
     Project = var.project
     Stage   = var.stage
+  }
+}
+
+resource "aws_s3_bucket" "infrastructure_bucket" {
+  name = var.infrastructure_bucket_name
+  tags = {
+    Project = var.project
+    Stage   = var.stage
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "infrastructure_bucket" {
+  bucket = aws_s3_bucket.infrastructure_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "infrastructure_bucket" {
+  bucket = aws_s3_bucket.infrastructure_bucket.id
+}
+
+resource "aws_s3_bucket_logging" "infrastructure_bucket" {
+  bucket        = aws_s3_bucket.infrastructure_bucket.id
+  target_bucket = aws_s3_bucket.logging_bucket.id
+  target_prefix = "infrastructure_bucket/"
+}
+
+data "template_file" "infrastructure_bucket_policy" {
+  template = file("infrastructure_bucket_policy.tpl")
+  vars = {
+    accountId = data.aws_caller_identity.current.account_id
+    bucketName = var.infrastructure_bucket_name
   }
 }
---
+++ infrastructure/lambdas/cloudwatch-to-s3.ts
@@ -0,0 +1,125 @@
+import {
+  CloudWatchLogsClient,
+  DescribeLogGroupsCommand,
+  DescribeLogGroupsRequest,
+  LogGroup,
+  ListTagsForResourceCommand,
+  CreateExportTaskCommand
+} from '@aws-sdk/client-cloudwatch-logs';
+import {
+  SSMClient,
+  GetParameterCommand,
+  PutParameterCommand
+} from '@aws-sdk/client-ssm';
+
+const logs = new CloudWatchLogsClient({});
+const ssm = new SSMClient({});
+const region = process.env.AWS_REGION || 'us-east-1';
+const accountId = process.env.AWS_ACCOUNT_ID || '957221700844';
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const handler = async () => {
+  const extra_args: DescribeLogGroupsRequest = {};
+  let log_groups: LogGroup[] = [];
+  const log_groups_to_export: string[] = [];
+
+  if (!process.env.S3_BUCKET) {
+    console.error('Error: S3_BUCKET not defined');
+    return;
+  }
+
+  console.log('--> S3_BUCKET=' + process.env.S3_BUCKET);
+
+  while (true) {
+    const response = await logs.send(new DescribeLogGroupsCommand(extra_args));
+    log_groups = log_groups.concat(response.logGroups);
+
+    if (!response.nextToken) {
+      break;
+    }
+    extra_args.nextToken = response.nextToken;
+  }
+
+  for (const log_group of log_groups) {
+    const command = new ListTagsForResourceCommand({
+      resourceArn: `arn:aws:logs:${region}:${accountId}:log-group:${log_group.logGroupName}`
+    });
+    const response = await logs.send(command);
+    const log_group_tags = response.tags || {};
+
+    if (log_group_tags.ExportToS3 === 'true') {
+      log_groups_to_export.push(log_group.logGroupName);
+    }
+    await delay(10 * 1000); // prevents LimitExceededException (AWS allows only one export task at a time)
+  }
+
+  for (const log_group_name of log_groups_to_export) {
+    const ssm_parameter_name = (
+      '/log-exporter-last-export/' + log_group_name
+    ).replace('//', '/');
+    let ssm_value = '0';
+
+    try {
+      const ssm_response = await ssm.send(
+        new GetParameterCommand({ Name: ssm_parameter_name })
+      );
+      ssm_value = ssm_response.Parameter?.Value || '0';
+    } catch (error) {
+      if (error.name !== 'ParameterNotFound') {
+        console.error('Error fetching SSM parameter: ' + error.message);
+      }
+    }
+
+    const export_to_time = Math.round(Date.now());
+
+    console.log(
+      '--> Exporting ' + log_group_name + ' to ' + process.env.S3_BUCKET
+    );
+
+    if (export_to_time - parseInt(ssm_value) < 24 * 60 * 60 * 1000) {
+      // Haven't been 24hrs from the last export of this log group
+      console.log('    Skipped until 24hrs from last export is completed');
+      continue;
+    }
+
+    try {
+      const response = await logs.send(
+        new CreateExportTaskCommand({
+          logGroupName: log_group_name,
+          from: parseInt(ssm_value),
+          to: export_to_time,
+          destination: process.env.S3_BUCKET,
+          destinationPrefix: log_group_name
+            .replace(/^\//, '')
+            .replace(/\/$/, '')
+        })
+      );
+
+      console.log('    Task created: ' + response.taskId);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    } catch (error) {
+      if (error.name === 'LimitExceededException') {
+        console.log(
+          '    Need to wait until all tasks are finished (LimitExceededException). Continuing later...'
+        );
+        return;
+      }
+      console.error(
+        '    Error exporting ' +
+          log_group_name +
+          ': ' +
+          (error.message || JSON.stringify(error))
+      );
+      continue;
+    }
+
+    await ssm.send(
+      new PutParameterCommand({
+        Name: ssm_parameter_name,
+        Type: 'String',
+        Value: export_to_time.toString(),
+        Overwrite: true
+      })
+    );
+  }
+};
