e5f0654a3ba2afea0c68d1c3274ba95708917f97
aloftus23 <andylofti@gmail.com>
2023-10-05 10:19:58 -0400

    Add handler and more logging to SQS lambda

--- backend/src/tasks/scanExecution.ts
+++ backend/src/tasks/scanExecution.ts
@@ -1,13 +1,16 @@
-import { SQSEvent, SQSRecord } from 'aws-lambda';
+import { Handler, SQSRecord } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
 
 const ecs = new AWS.ECS();
 const sqs = new AWS.SQS();
 
-export const invokeFargateTask = async (event: SQSEvent): Promise<any> => {
+export const handler: Handler = async (event) => {
   try {
+    // Get the SQS record and message body
     const sqsRecord: SQSRecord = event.Records[0];
     const commandOptions: string = sqsRecord.body;
+
+    console.log(commandOptions);
 
     // Get the ARN of the SQS queue from the event
     const sqsQueueArn: string | undefined = sqsRecord.eventSourceARN;
@@ -24,11 +27,12 @@ export const invokeFargateTask = async (event: SQSEvent): Promise<any> => {
       .getQueueAttributes(sqsQueue)
       .promise();
     const sqsQueueUrl = queueAttributesResponse.Attributes?.QueueUrl;
-
+    console.log(sqsQueueUrl);
     if (!sqsQueueUrl) {
       throw new Error('SQS Queue URL not found');
     }
 
+    // Run command in queue message in Fargate
     const params: AWS.ECS.RunTaskRequest = {
       cluster: process.env.FARGATE_CLUSTER_NAME!,
       taskDefinition: process.env.FARGATE_TASK_DEFINITION_NAME!,
@@ -50,7 +54,6 @@ export const invokeFargateTask = async (event: SQSEvent): Promise<any> => {
         ]
       }
     };
-
     const data = await ecs.runTask(params).promise();
     console.log('Fargate task started:', data);
 
