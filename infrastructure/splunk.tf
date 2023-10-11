1d23335b77df55a8020731ff9592c9b1815da709
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-10-11 14:22:10 -0500

    Update Splunk assume_role policy.

--- infrastructure/splunk.tf
+++ infrastructure/splunk.tf
@@ -3,11 +3,11 @@ resource "aws_iam_role" "splunk_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Action = "sts:AssumeRole",
       Effect = "Allow",
       Principal = {
-        Service = "s3.amazonaws.com"
+        AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
       }
+      Action = "sts:AssumeRole",
     }]
   })
   tags = {
@@ -15,6 +15,7 @@ resource "aws_iam_role" "splunk_role" {
     Stage   = var.stage
   }
 }
+#TODO: add assume_role_policy to splunk service user: "Resource": "arn:aws:iam::123456789012:role/${var.splunk_role_name}
 
 resource "aws_iam_role_policy" "splunk_policy" {
   name_prefix = "crossfeed-splunk-s3-${var.stage}"
