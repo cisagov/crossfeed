3cff819d61c5e84da937ad420fb943e2b95e3bf9
Courtneyc1 <126171978+courtneyc1@users.noreply.github.com>
2023-10-13 15:17:38 -0400

    npm test -u

--- frontend/src/components/Footer/styleFooter.ts
+++ frontend/src/components/Footer/styleFooter.ts
@@ -11,7 +11,7 @@ export const footerClasses = {
 };
 export const FooterRoot = styled('div')(({ theme }) => ({
   [`& .${footerClasses.footerBox}`]: {
-    position: 'sticky',
+    position: 'absolute',
     bottom: 0,
     width: '100%',
     backgroundColor: theme.palette.primary.main
