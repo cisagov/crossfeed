54f406d65b72cb6ea720ab1077c014e5f7dee521
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-10-17 11:46:10 -0500

    Fix SSMParameterName to match hierarchical naming convention.

--- backend/src/tasks/cloudwatchToS3.ts
+++ backend/src/tasks/cloudwatchToS3.ts
@@ -57,10 +57,8 @@ export const handler = async () => {
     }
     const logGroupName = logGroup.logGroupName!;
     console.log(`Processing log group: ${logGroupName}`);
-    const ssmParameterName = `last-export-to-s3/${logGroupName}`.replace(
-      '//',
-      '/'
-    );
+    const ssmParameterName =
+      `/logs/${stage}/last-export-to-s3/${logGroupName}`.replace('//', '/');
     let ssmValue = '0';
 
     try {
