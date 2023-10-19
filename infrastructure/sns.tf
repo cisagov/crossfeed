27909bbe4679ae65c83eb10d5c63bbdd4f9c6a89
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-10-19 09:18:42 -0500

    Fix topic arn for sns topic subscription.

--- infrastructure/sns.tf
+++ infrastructure/sns.tf
@@ -5,5 +5,5 @@ resource "aws_sns_topic" "alarms" {
 resource "aws_sns_topic_subscription" "alarms" {
   endpoint  = aws_sqs_queue.alarms.arn
   protocol  = "sqs"
-  topic_arn = aws_sns_topic.alarms
+  topic_arn = aws_sns_topic.alarms.arn
 }
