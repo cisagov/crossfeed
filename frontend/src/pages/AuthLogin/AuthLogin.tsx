9362ae75ea698517cd8c6dc49d3050ff378c0b6a
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-10-19 16:07:44 -0500

    Add notification pane.

--- frontend/src/pages/AuthLogin/AuthLogin.tsx
+++ frontend/src/pages/AuthLogin/AuthLogin.tsx
@@ -93,6 +93,62 @@ export const AuthLogin: React.FC<{ showSignUp?: boolean }> = ({
               !showSignUp && !(process.env.NODE_ENV === 'development')
             }
           />
+          <div className="notification_box">
+            <div className="platform_header">PLATFORM NOTIFICATION</div>
+            <div className="notification_header">
+              Important Notice: Temporary Downtime During Crossfeed Migration
+            </div>
+            <div className="temp_notification">
+              {' '}
+              The Crossfeed environment is moving. The migration will require a
+              temporary downtime of approximately one week.
+            </div>
+            <div className="temp_notification">
+              {' '}
+              The downtime will begin on Wednesday, October 25, through the end
+              of day Wednesday, November 01.{' '}
+            </div>
+            <div className="temp_notification">
+              For additional information, please click{' '}
+              <a href="https://s3.amazonaws.com/staging.crossfeed.cyber.dhs.gov/Notice.pdf">
+                [here]
+              </a>
+            </div>
+          </div>
+          <div className="banner_box">
+            <div className="banner_header">**Warning**</div>
+            <div className="banner_login">
+              {' '}
+              This system contains U.S. Government Data. Unauthorized use of
+              downtime will begin on Monday, October 23, through the end of
+              Monday, Monday, Monday, Monday, October 30. unauthorized,
+              constitutes system.
+            </div>
+            <div className="banner_login">
+              {' '}
+              This computer system, including all related equipment, networks,
+              (specifically including Internet access) are provided only for
+              authorized U.S. Government use. U.S. Government computer systems
+              monitored for all lawful purposes, authorized, for management of
+              the system, system, to system, to system, to facilitate protection
+              against unauthorized access, and verify security procedures,
+              procedures, procedures, procedures, survivability, operational
+              operational operational operational Monitoring includes active
+              attacks by authorized U.S. Government entities to test or verify
+              the security the security of this system. During monitoring,
+              monitoring, information may be examined, examined, recorded,
+              copied and used for authorized information, including personal
+              information, placed or sent over or sent over or sent over this
+              system may be may be may be monitored.
+            </div>
+            <div className="banner_login">
+              {' '}
+              Unauthorized use may subject you to criminal prosecution. Evidence
+              unauthorized use collected during monitoring may be used for
+              administrative, criminal, or other adverse action. Use of this
+              constitutes consent to monitoring for these purposes.
+            </div>
+          </div>
         </ThemeProvider>
       </AuthForm>
     );
@@ -105,7 +161,6 @@ export const AuthLogin: React.FC<{ showSignUp?: boolean }> = ({
       <Button type="submit" size="big">
         Login with Login.gov
       </Button>
-      <br></br>
       <Link to="#" onClick={onSubmit}>
         New to Crossfeed? Register with Login.gov
       </Link>
