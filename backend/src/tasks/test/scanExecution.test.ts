df77293e792cd48af005e986783d0e850df96309
aloftus23 <andylofti@gmail.com>
2023-11-06 08:43:11 -0500

    Update scanExecution test

--- backend/src/tasks/test/scanExecution.test.ts
+++ backend/src/tasks/test/scanExecution.test.ts
@@ -1,38 +1,32 @@
-import { handler } from '../scanExecution';
+import { handler as scanExecution } from '../scanExecution';
 import { SQSRecord } from 'aws-lambda';
-
-// Mock the AWS SDK methods using aws-sdk-mock
-jest.mock('aws-sdk', () => {
-  return {
-    ECS: jest.fn(() => ({
-      runTask: jest.fn().mockReturnThis(),
-      promise: jest.fn()
-    })),
-    SQS: jest.fn(() => ({
-      sendMessage: jest.fn().mockReturnThis(),
-      promise: jest.fn()
-    }))
-  };
-});
+import * as AWS from 'aws-sdk';
 
 describe('Scan Execution', () => {
+  beforeEach(() => {
+    // Mock the sendMessage method manually
+    const mockSendMessage = jest.fn().mockReturnValue({
+      promise: jest.fn().mockResolvedValue({})
+    });
+
+    AWS.SQS.prototype.sendMessage = mockSendMessage;
+  });
+
   process.env.SQS_QUEUE_URL = 'YOUR_SQS_QUEUE_URL';
-  it('should handle the event', async () => {
+
+  it('scanExecution should run successfully', async () => {
     const event = {
       Records: [
         {
-          body: 'test command',
+          body: JSON.stringify({ scriptType: 'shodan' }),
           eventSourceARN: 'SQSQueueARN'
         } as SQSRecord
       ]
     };
 
-    const result = await handler(event, {} as any, () => void 0);
+    const result = await scanExecution(event, {} as any, () => void 0);
 
-    // Add your assertions here
     expect(result.statusCode).toEqual(200);
-    expect(result.body).toContain(
-      'Fargate task started and message sent to SQS queue'
-    );
+    expect(result.body).toContain('Fargate task started and message sent to SQS queue');
   });
 });
