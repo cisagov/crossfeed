ae709d69aabb7de9d8e97639520885dc7eca257a
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2023-11-07 06:50:14 -0600

    Add http response headers to docs page for cors, hsts, csp, and xFrameOptions.

--- frontend/scripts/docs.js
+++ frontend/scripts/docs.js
@@ -2,6 +2,8 @@ import serverless from 'serverless-http';
 import express from 'express';
 import path from 'path';
 import rateLimit from 'express-rate-limit';
+import cors from 'cors';
+import helmet from 'helmet';
 
 export const app = express();
 
@@ -13,7 +15,23 @@ app.use(
 ); // limit 1000 requests per 15 minutes
 
 app.use(express.static(path.join(__dirname, '../docs/build')));
+app.use(cors());
 
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        baseUri: ["'none'"],
+        defaultSrc: ["'self'"],
+        frameAncestors: ["'none'"],
+        objectSrc: ["'none'"],
+        scriptSrc: ["'none'"]
+      }
+    },
+    hsts: { maxAge: 31536000, preload: true },
+    xFrameOptions: 'DENY'
+  })
+);
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../docs/build/index.html'));
 });
