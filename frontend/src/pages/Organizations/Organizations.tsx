4582e7bd51e3d3cb3b46e6a8c956df1d65fedf7d
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2024-01-09 13:59:12 -0500

    Fix lint errors related to eslint-plugin-prettier update.

--- frontend/src/pages/Organizations/Organizations.tsx
+++ frontend/src/pages/Organizations/Organizations.tsx
@@ -103,7 +103,7 @@ export const Organizations: React.FC = () => {
                     ({
                       ...org,
                       tags: org.tags.map((tag) => tag.name)
-                    } as any)
+                    }) as any
                 )
               }
             />
