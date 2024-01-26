68013d60f99256d0fbdab09f10973cd71ce37d94
nickviola <nick.viola@associates.cisa.dhs.gov>
2024-01-26 14:09:34 -0600

    Add permission check to RouteGuard Compenent in RouteGuard.tsx

--- frontend/src/components/RouteGuard.tsx
+++ frontend/src/components/RouteGuard.tsx
@@ -1,9 +1,11 @@
+// import React, { useEffect } from 'react';
 import React from 'react';
 import { RouteProps, Route, useHistory } from 'react-router-dom';
 import { useAuthContext } from 'context';
 
 interface AuthRedirectRouteProps extends RouteProps {
   unauth?: string | React.ComponentType;
+  permissions?: Array<String>;
   component: React.ComponentType;
 }
 
@@ -15,14 +17,16 @@ possible states:
 - user has authenticated but needs to sign terms
 - user is not authenticated
 - user is not authenticated, this is oauth callback (should not be protected)
+- user is authenticated, but does not have tha correct permissions for route
 */
 
 export const RouteGuard: React.FC<AuthRedirectRouteProps> = ({
   unauth = '/',
+  permissions = [],
   component,
   ...rest
 }) => {
-  const { token, user, userMustSign } = useAuthContext();
+  const { token, user, userMustSign, logout } = useAuthContext();
   const history = useHistory();
 
   if (token && !user) {
@@ -45,6 +49,18 @@ export const RouteGuard: React.FC<AuthRedirectRouteProps> = ({
   if (typeof unauth === 'string' && !user) {
     history.push(unauth);
     return null;
+  }
+
+  if (user && permissions && permissions.length > 0) {
+    // user is not globalAdmin and invalid userType permissions
+    if (
+      user.userType !== 'globalAdmin' &&
+      !permissions.includes(user.userType)
+    ) {
+      console.log('User access denied. Logging out!');
+      // Log User out
+      logout();
+    }
   }
 
   return (
