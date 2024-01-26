88ee1f154a34e5360c6a8b48365b8b75753bb0dc
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2024-01-26 10:10:39 -0600

    Create project constants file and import it in backend/tsconfig.json. Create constant for CORS options to reduce code duplication.

--- constants.ts
+++ constants.ts
@@ -1,3 +1,5 @@
+// TODO: Explore alternate ways to implement DRY
+
 const CORS_OPTIONS = {
   origin: [/crossfeed\.cyber\.dhs\.gov$/, /localhost$/],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
