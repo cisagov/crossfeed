0602719779cf8091646836996f209a10e9c90124
Grayson, Matthew <matthew.grayson@associates.cisa.dhs.gov>
2024-01-30 13:35:34 -0600

    Force pdf.save to wait for canvas image to fully load.

--- frontend/src/pages/Risk/Risk.tsx
+++ frontend/src/pages/Risk/Risk.tsx
@@ -223,33 +223,37 @@ const Risk: React.FC = (props) => {
     const dateTimeNow = new Date(); // UTC Date Time
     const localDate = new Date(dateTimeNow); // Local Date Time
     setIsLoading(true);
-    await delay(650);
     const input = document.getElementById('wrapper')!;
     input.style.width = '1400px';
-    await delay(1);
     await html2canvas(input, {
       scrollX: 0,
       scrollY: 0,
       ignoreElements: function (element) {
         return 'mapWrapper' === element.id;
       }
-    }).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const imgWidth = 190;
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
-      const pdf = new jsPDF('p', 'mm');
-      pdf.setFontSize(18);
-      pdf.text('Crossfeed Report', 12, 10);
-      pdf.setFontSize(10);
-      pdf.text(dateTimeNow.toISOString(), 12, 17);
-      pdf.addImage(imgData, 'PNG', 10, 20, imgWidth, imgHeight); // charts
-      pdf.line(3, 290, 207, 290);
-      pdf.setFontSize(8);
-      pdf.text('Prepared by ' + user?.fullName + ', ' + localDate, 3, 293); // print the name of the person who printed the report as well as a human friendly date/time
-      pdf.save('Crossfeed_Report_' + dateTimeNow.toISOString() + '.pdf'); // sets the filename and adds the date and time
-    });
-    input.style.removeProperty('width');
-    setIsLoading(false);
+    })
+      .then((canvas) => {
+        const pdf = new jsPDF('p', 'mm');
+        const img = new Image();
+        const imgWidth = 190;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        img.src = canvas.toDataURL('image/png');
+        img.onload = () => {
+          pdf.setFontSize(18);
+          pdf.text('Crossfeed Report', 12, 10);
+          pdf.setFontSize(10);
+          pdf.text(dateTimeNow.toISOString(), 12, 17);
+          pdf.addImage(img, 'PNG', 10, 20, imgWidth, imgHeight); // charts
+          pdf.line(3, 290, 207, 290);
+          pdf.setFontSize(8);
+          pdf.text('Prepared by ' + user?.fullName + ', ' + localDate, 3, 293); // print the name of the person who printed the report as well as a human friendly date/time
+          pdf.save('Crossfeed_Report_' + dateTimeNow.toISOString() + '.pdf'); // sets the filename and adds the date and time
+        };
+      })
+      .then(() => {
+        input.style.removeProperty('width');
+        setIsLoading(false);
+      });
   };
 
   return (
