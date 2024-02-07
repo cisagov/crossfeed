f237384d289f56b2a61e7a5d5af1e5c05d27784e
Amelia Vance <amelia.vance@associates.cisa.dhs.gov>
2024-02-06 19:10:19 -0500

    Add product info to Vulnerability.tsx

--- frontend/src/types/product-info.ts
+++ frontend/src/types/product-info.ts
@@ -3,7 +3,7 @@ export interface ProductInfo {
   id: string;
   cpe_product_name: string;
   version_number: string;
-  vender: string;
+  vender?: string | any;
   last_seen: Date;
   cve: Cve[];
 }
