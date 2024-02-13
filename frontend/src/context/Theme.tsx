ddd261e5c488eec32baa57813e48f92fa4b38a1c
Thomas Johnson <123108455+hawkishpolicy@users.noreply.github.com>
2024-02-12 21:45:52 -0500

    created a new theme provider and new theme context to allow switching between dark and light modes.

--- frontend/src/context/Theme.tsx
+++ frontend/src/context/Theme.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
   createTheme,
   ThemeProvider,
@@ -11,38 +11,90 @@ declare module '@mui/material/styles' {
   interface DefaultTheme extends Theme {}
 }
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#07648D'
-    },
-    secondary: {
-      main: '#28A0CB'
-    },
-    background: {
-      default: '#EFF1F5'
-    }
-  },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 960,
-      lg: 1330,
-      xl: 1920
-    }
-  }
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#07648D'
+//     },
+//     secondary: {
+//       main: '#28A0CB'
+//     },
+//     background: {
+//       default: '#EFF1F5'
+//     }
+//   },
+//   breakpoints: {
+//     values: {
+//       xs: 0,
+//       sm: 600,
+//       md: 960,
+//       lg: 1330,
+//       xl: 1920
+//     }
+//   }
+// });
+
+type CFThemeContextType = {
+  switchDarkMode: () => void;
+};
+
+export const CFThemeContext = React.createContext<CFThemeContextType>({
+  switchDarkMode: () => {}
 });
+
+export function CFThemeProvider({ children }: CFThemeProviderProps) {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+
+  const switchDarkMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          primary: {
+            main: '#07648D'
+          },
+          secondary: {
+            main: '#28A0CB'
+          },
+          background: {
+            default: '#EFF1F5'
+          }
+        },
+        breakpoints: {
+          values: {
+            xs: 0,
+            sm: 600,
+            md: 960,
+            lg: 1330,
+            xl: 1920
+          }
+        }
+      }),
+    [mode]
+  );
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <CFThemeContext.Provider value={{ switchDarkMode }}>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </CFThemeContext.Provider>
+    </StyledEngineProvider>
+  );
+}
 
 interface CFThemeProviderProps {
   children: React.ReactNode;
 }
-export const CFThemeProvider: React.FC<CFThemeProviderProps> = ({
-  children
-}) => {
-  return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </StyledEngineProvider>
-  );
-};
+// export const CFThemeProvider: React.FC<CFThemeProviderProps> = ({
+//   children
+// }) => {
+//   return (
+//     <StyledEngineProvider injectFirst>
+//       <ThemeProvider theme={theme}>{children}</ThemeProvider>
+//     </StyledEngineProvider>
+//   );
+// };
